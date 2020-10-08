d858b9934ffcdf8a4357b2f67be2d37508df0318
ravi <rkgeekoftheweek@gmail.com>
2020-10-08 17:07:24 +0530

    ADDED : QR Code Scanner

---
+++ QR_Code_Scanner/Readme.md
@@ -0,0 +1,12 @@
+Install following packages in before running code
+
+1. pip3 install pyzbar
+2. pip3 install Pillow
+3. apt-get install python-tk
+4. apt-get install zbar-tools
+
+How to Run it :  python3 scanner.py
+
+QR CODE SCANNER window will pop up.
+select QR code image file which you want to scan.
+Result will be displayed in  same window.
---
+++ QR_Code_Scanner/scanner.py
@@ -0,0 +1,31 @@
+from pyzbar.pyzbar import decode
+from PIL import Image, ImageTk
+from tkinter import *
+from tkinter import filedialog
+import PIL.Image
+import tkinter.font as tkFont
+
+root = Tk()
+root.title('QR CODE SCANNER')
+root.geometry("600x400")
+
+
+def open():
+    global my_image
+    root.filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("png files","*.png"),("all files","*.*")))
+    my_label = Label(root,text=root.filename).pack()
+    my_image= ImageTk.PhotoImage(PIL.Image.open(root.filename))
+    my_image_label = Label(image=my_image).pack()
+    result = decode(PIL.Image.open(root.filename))
+    data = result[0].data.decode()
+    data =" RESULT : {0}".format(data)
+    fontStyle = tkFont.Font(family="Lucida Grande", size=15)
+    data_label = Label(root , text=data, font=fontStyle ).pack()
+    
+    
+my_btn = Button(root, text="Select QR code image", command=open).pack()
+
+
+root.mainloop()
+
+
