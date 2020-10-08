9d8fbc458706232cae9b1dc6b0dccbd8c0f168bb
parth93QA <parth.zero93@gmail.com>
2020-10-08 11:45:09 +0530

    modified code

--- html_to_pdf/htmtopdf.py
+++ html_to_pdf/htmtopdf.py
@@ -10,7 +10,6 @@ fr_url = input('Enter the url the html of which will be converted to pdf : ')
 to_pdf = input('Enter the output of the file without pdf extension :')
 
 try:
-	pdfkit.from_url(fr_url,to_pdf+'.pdf')
+	pdfkit.from_url(fr_url, to_pdf + '.pdf')
 except:
 	print("Some error occured while converting to pdf. Please check entered url and output file format")
-
