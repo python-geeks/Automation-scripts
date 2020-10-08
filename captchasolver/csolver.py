cf65da64bad1eb7c67c867ab6e445bafdb1bd0f1
gkydev <gokaymrc@hotmail.com>
2020-10-08 17:06:41 +0300

    Update csolver.py

--- captchasolver/csolver.py
+++ captchasolver/csolver.py
@@ -1,26 +1,30 @@
-import pytesseract, sys, os
+import pytesseract
+import sys
+import os
 from PIL import Image
 
-#Set Tesserat location
+# Set Tesseract location
 pytesseract.pytesseract.tesseract_cmd = ""
 
-#Image 
-im =  sys.argv[1]
+# Image
+im = sys.argv[1]
+
 
 def resize_image(img):
     img = Image.open(img)
-    img = img.resize((1000,500),Image.NEAREST)
+    img = img.resize((1000, 500), Image.NEAREST)
     img.save("resized.png")
 
+
 def solve(img):
-    #First resize captcha and try to read it 
     resize_image(img)
     cr = pytesseract.image_to_string("resized.png")
     os.remove("resized.png")
+    # First resize captcha and try to read it.
     if cr != "\x0c":
         print(cr)
     else:
-        #If it cant read try to read in normal size   
+        # If it cant read try to read in normal size
         cr = pytesseract.image_to_string(im)
         print(cr)
 # Run
