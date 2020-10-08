a74f8c2aec483b255cb12be25686dedf5d67ab1e
fieryash <ashtikkm@gmail.com>
2020-10-08 22:43:17 +0530

    fixed linting issues

--- image_colour_to_bw/colour_to_bw.py
+++ image_colour_to_bw/colour_to_bw.py
@@ -1,4 +1,6 @@
 import cv2
+
+
 def colour_to_bw(original_image_path, output_path):
     '''
     Function to convert colour image to black and white
@@ -6,5 +8,5 @@ def colour_to_bw(original_image_path, output_path):
     original_image = cv2.imread(original_image_path)
     gray_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
     thresh = 128
-    img_bw = cv2.threshold(gray_img,thresh,255, cv2.THRESH_BINARY)[1]
-    cv2.imwrite(output_path+'final_image.png',img_bw)
+    img_bw = cv2.threshold(gray_img, thresh, 255, cv2.THRESH_BINARY)[1]
+    cv2.imwrite(output_path+'final_image.png', img_bw)
