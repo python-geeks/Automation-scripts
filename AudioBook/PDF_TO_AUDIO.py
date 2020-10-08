d0a8cceb62f0eb068f5a5d9e6eeb2e084069c549
Aakash Agarwal <58568036+Aakasha01Agarwal@users.noreply.github.com>
2020-10-08 10:27:12 +0530

    added pdf to audiobook

--- AudioBook/PDF_TO_AUDIO.py
+++ AudioBook/PDF_TO_AUDIO.py
@@ -3,25 +3,25 @@ import PyPDF2
 from gtts import gTTS
 from tkinter.filedialog import askopenfilename
 from tkinter import Tk
-Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-filelocation = askopenfilename() # open the dialog GUI
+Tk().withdraw()
+filelocation = askopenfilename()  # open the dialog GUI
 
 
-book=open(filelocation,'rb')
+book = open(filelocation, 'rb')
 
 pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
 print("No. of pages: ", pages)
 speaker = pyttsx3.init()
-whole_text=''
-choice=input('choice? ')
+whole_text = ''
+choice = input('choice? ')
 
-if choice=='1':
+if choice == '1':
 
     for num in range(0, pages):
         page = pdfReader.getPage(num)
         text = page.extractText()
-        whole_text+=text
+        whole_text += text
         speaker.say(text)
         print("Reading: ", text)
         speaker.runAndWait()
