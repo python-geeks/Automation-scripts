939f88de1521d562262ab983e59b68ea7aeb167a
Ravi K <50319065+rkgeekoftheweek@users.noreply.github.com>
2020-10-08 17:56:45 +0530

    Update scanner.py

--- QR_Code_Scanner/scanner.py
+++ QR_Code_Scanner/scanner.py
@@ -12,20 +12,22 @@ root.geometry("600x400")
 
 def open():
     global my_image
-    root.filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("png files","*.png"),("all files","*.*")))
-    my_label = Label(root,text=root.filename).pack()
-    my_image= ImageTk.PhotoImage(PIL.Image.open(root.filename))
+    root.filename = filedialog.askopenfilename(
+      initialdir='/home',
+      title="Select file",
+      filetypes=(
+        ("png files", "*.png"),
+        ("all files", "*.*")
+                )
+    )
+    my_label = Label(root, text=root.filename).pack()
+    my_image = ImageTk.PhotoImage(PIL.Image.open(root.filename))
     my_image_label = Label(image=my_image).pack()
     result = decode(PIL.Image.open(root.filename))
     data = result[0].data.decode()
-    data =" RESULT : {0}".format(data)
+    data = " RESULT : {0}".format(data)
     fontStyle = tkFont.Font(family="Lucida Grande", size=15)
-    data_label = Label(root , text=data, font=fontStyle ).pack()
-    
-    
+    data_label = Label(root, text=data, font=fontStyle).pack()
 my_btn = Button(root, text="Select QR code image", command=open).pack()
 
-
 root.mainloop()
-
-
