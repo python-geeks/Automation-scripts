cee71d151b462cf9eb57b6bfe249a0dc27cd192b
Tassatir <gokaymrc@hotmail.com>
2020-10-08 16:15:00 +0300

    Added simple captcha solver script

---
+++ captchasolver/README.md
@@ -0,0 +1,24 @@
+# Captcha Solver
+A simple python script for solving simple captchas
+
+## Modules Used
+
+- pytesseract
+- sys
+- os
+- PIL
+
+## Requirements
+
+- tesseract
+`You can download tesseract from [here](https://tesseract-ocr.github.io/tessdoc/Home.html)`
+- pytesseract
+`pip install pytesseract`
+
+- After you install tesseract replace 
+`# Your tesseract location `
+with your tesseract location in csolver.py
+
+## How to use
+`python csolver.py captcha_image`
+
---
+++ captchasolver/csolver.py
@@ -0,0 +1,27 @@
+import pytesseract, sys, os
+from PIL import Image
+
+#Set Tesserat location
+pytesseract.pytesseract.tesseract_cmd = # Your tesseract location
+
+#Image 
+im =  sys.argv[1]
+
+def resize_image(img):
+    img = Image.open(img)
+    img = img.resize((1000,500),Image.NEAREST)
+    img.save("resized.png")
+
+def solve(img):
+    #First resize captcha and try to read it 
+    resize_image(img)
+    cr = pytesseract.image_to_string("resized.png")
+    os.remove("resized.png")
+    if cr != "\x0c":
+        print(cr)
+    else:
+        #If it cant read try to read in normal size   
+        cr = pytesseract.image_to_string(im)
+        print(cr)
+# Run
+solve(im)
