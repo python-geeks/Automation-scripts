3ea4d49ed7d67abbb8b5c6a548f5145e8ba6a97a
fieryash <ashtikkm@gmail.com>
2020-10-08 22:33:16 +0530

    added Readme.md and requirements.txt

--- image_colour_to_bw/colour_to_bw.py
+++ image_colour_to_bw/colour_to_bw.py
@@ -7,4 +7,4 @@ def colour_to_bw(original_image_path, output_path):
     gray_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
     thresh = 128
     img_bw = cv2.threshold(gray_img,thresh,255, cv2.THRESH_BINARY)[1]
-    cv2.imwrite(output_path+'\final_image.png',img_bw)
+    cv2.imwrite(output_path+'final_image.png',img_bw)
