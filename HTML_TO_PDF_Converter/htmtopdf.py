fe9b804bd133b41302d9763b5d2edc19bbe34628
parth93QA <parth.zero93@gmail.com>
2020-10-08 11:35:36 +0530

    add html to pdf script

---
+++ HTML_TO_PDF_Converter/htmtopdf.py
@@ -0,0 +1,16 @@
+#Install python3
+#Install pip
+#Install pdfkit --> pip install pdfkit
+#Install wkhtmltopdf ---> https://wkhtmltopdf.org/downloads.html depedning on your operating system.
+
+import pdfkit
+import os
+
+fr_url = input('Enter the url the html of which will be converted to pdf : ')
+to_pdf = input('Enter the output of the file without pdf extension :')
+
+try:
+	pdfkit.from_url(fr_url,to_pdf+'.pdf')
+except:
+	print("Some error occured while converting to pdf. Please check entered url and output file format")
+
