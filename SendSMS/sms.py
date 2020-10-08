1078c3af3bdcb78d8a5eb094bffbfb554fa95a02
yourfriendlyerror <41340245+YourFriendlyError@users.noreply.github.com>
2020-10-08 14:08:47 -0400

    Updated sms.py
    Converted tabs to spaces.

--- SendSMS/sms.py
+++ SendSMS/sms.py
@@ -11,27 +11,26 @@ details = {'Message created': '', 'Date sent': '', 'Body': ''}
 
 
 def craft(msg, fromPhone, sendTo):
-	message = client.messages.create(
-		body=msg,
-		from_=fromPhone,
-		to=sendTo
-	)
+    message = client.messages.create(
+        body=msg,
+        from_=fromPhone,
+        to=sendTo
+    )
 
-	details['Message created'] = str(message.date_created)
-	details['Date sent'] = str(message.date_sent)
-	details['Body'] = str(message.body)
+    details['Message created'] = str(message.date_created)
+    details['Date sent'] = str(message.date_sent)
+    details['Body'] = str(message.body)
 
 
 def main(msg: str):
-	return craft(msg, twilio_number, '+15555550100')
+    return craft(msg, twilio_number, '+15555550100')
 
 
 if __name__ == '__main__':
-	main('Hello, user! Sent from Python!')
-	seeDetails = input('See details of this message? [y/n]: ')
-	if seeDetails.lower() == 'y':
-		for i in details:
-			print(f'{i} = {details[i]}')
-	else:
-		exit()
-
+    main('Hello, user! Sent from Python!')
+    seeDetails = input('See details of this message? [y/n]: ')
+    if seeDetails.lower() == 'y':
+        for i in details:
+            print(f'{i} = {details[i]}')
+    else:
+        exit()
