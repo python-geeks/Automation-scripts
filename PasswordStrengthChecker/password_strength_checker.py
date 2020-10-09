572d7c2a9b54ad42a4a428a4c3f37d70dc424175
falguni-k <49592710+falguni-k@users.noreply.github.com>
2020-10-09 15:56:27 +0530

    Update password_strength_checker.py

--- PasswordStrengthChecker/password_strength_checker.py
+++ PasswordStrengthChecker/password_strength_checker.py
@@ -5,7 +5,7 @@ password = sys.argv[1]
 regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
 
 t1 = len(password) >= 8
-t2 = (regex.search(password) is not None)
+t2 = not (regex.search(password) is None)
 t3 = any(c.islower() for c in password)
 t4 = any(c.isupper() for c in password)
 t5 = any(c.isdigit() for c in password)
