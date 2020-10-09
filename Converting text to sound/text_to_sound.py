9971ba64afe0ecd8d1d48cc8cfce384c22bcb3ad
Anustup Mukherjee <60361231+Anustup900@users.noreply.github.com>
2020-10-09 19:36:44 +0530

    Update text_to_sound.py

--- Converting text to sound/text_to_sound.py
+++ Converting text to sound/text_to_sound.py
@@ -1,7 +1,7 @@
 from gtts import gTTS
 from playsound import playsound
 text_val = 'All the best for your exam.'
-language = 'en' 
+language = 'en'
 obj = gTTS(text=text_val, lang=language, slow=False)
-obj.save("exam.mp3")  
+obj.save("exam.mp3")
 playsound("exam.mp3")
