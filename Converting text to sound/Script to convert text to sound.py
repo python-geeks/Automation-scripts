eef6580ff811b70a31487ff7234a7dee17844177
Anustup Mukherjee <60361231+Anustup900@users.noreply.github.com>
2020-10-09 13:26:35 +0530

    Update Script to convert text to sound.py

--- Converting text to sound/Script to convert text to sound.py
+++ Converting text to sound/Script to convert text to sound.py
@@ -3,26 +3,44 @@ Created on Fri Oct  9 11:40:35 2020
 
 @author: Anustup
 """
-from tkinter import tkinter
+## import libraries
+
+from tkinter import *
 from gtts import gTTS
 from playsound import playsound
 
-page=Tk()
-page.geometry('370x370')
-page.resizable(0,0)
-page.config( bg ='ghost white')
-page.title('Contribution - Text_to_Speech')
 
-Label(page, text = 'Text_to_Speech' , font='arial 20 bold' , bg ='white smoke').pack()
-Label(page, text ='Contribution' , font ='arial 15 bold', bg = 'white smoke').pack(side = BOTTOM)
 
-Label(page, text ='Enter Text', font ='arial 15 bold', bg ='white smoke').place(x=20,y=60)
+################### Initialized window####################
+
+root = Tk()
+root.geometry('350x300')
+root.resizable(0,0)
+root.config(bg = 'ghost white')
+root.title('DataFlair - TEXT_TO_SPEECH')
+
+
+##heading
+Label(root, text = 'TEXT_TO_SPEECH' , font='arial 20 bold' , bg ='white smoke').pack()
+Label(root, text ='DataFlair' , font ='arial 15 bold', bg = 'white smoke').pack(side = BOTTOM)
+
+
+
+
+#label
+Label(root, text ='Enter Text', font ='arial 15 bold', bg ='white smoke').place(x=20,y=60)
+
+
 ##text variable
 Msg = StringVar()
 
-entry_field = Entry(page,textvariable =Msg, width ='50')
+
+#Entry
+entry_field = Entry(root,textvariable =Msg, width ='50')
 entry_field.place(x=20 , y=100)
 
+
+###################define function##############################
 
 def Text_to_speech():
     Message = entry_field.get()
@@ -31,15 +49,15 @@ def Text_to_speech():
     playsound('DataFlair.mp3')
 
 def Exit():
-    page.destroy()
+    root.destroy()
 
 def Reset():
     Msg.set("")
 
 #Button
-Button(page, text = "PLAY" , font = 'arial 15 bold', command = Text_to_speech, width =4).place(x=25, y=140)
-Button(page,text = 'EXIT',font = 'arial 15 bold' , command = Exit, bg = 'OrangeRed1').place(x=100,y=140)
-Button(page, text = 'RESET', font='arial 15 bold', command = Reset).place(x=175 , y =140)
+Button(root, text = "PLAY" , font = 'arial 15 bold', command = Text_to_speech, width =4).place(x=25, y=140)
+Button(root,text = 'EXIT',font = 'arial 15 bold' , command = Exit, bg = 'OrangeRed1').place(x=100,y=140)
+Button(root, text = 'RESET', font='arial 15 bold', command = Reset).place(x=175 , y =140)
 
 
 #infinite loop to run program
