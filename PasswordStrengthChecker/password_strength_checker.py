f203ed216b065fe6dd8baba6b869c31aa501e91c
falguni-k <49592710+falguni-k@users.noreply.github.com>
2020-10-09 15:53:46 +0530

    Update password_strength_checker.py

--- PasswordStrengthChecker/password_strength_checker.py
+++ PasswordStrengthChecker/password_strength_checker.py
@@ -2,15 +2,15 @@ import sys
 import re
 
 password = sys.argv[1]
-regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]') 
+regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
 
-t1 = len(password)>=8
-t2 = not (regex.search(password) == None)
+t1 = len(password) >= 8
+t2 = (regex.search(password) is not None)
 t3 = any(c.islower() for c in password)
 t4 = any(c.isupper() for c in password)
 t5 = any(c.isdigit() for c in password)
 
 if t1 and t2 and t3 and t4 and t5:
-	print("Password is STRONG")
+    print("Password is STRONG")
 else:
-	print("Password is WEAK")
+    print("Password is WEAK")
