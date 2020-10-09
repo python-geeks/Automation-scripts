5d58298f9dd466fbd231ffead1c7d7d2192c6da6
Ashkan Kamyab <ashkankamyab@gmail.com>
2020-10-09 17:42:30 +0200

    Added docstring comment for classes and functions

--- tweetBot/favretweet.py
+++ tweetBot/favretweet.py
@@ -8,31 +8,51 @@ from config import create_api
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
-
+"""
+Resuable api object from config.py.
+Tweepy to uses sub-classes of tweepy ojects within FavRetweetListener class.
+Tweepy stream to actively watch for tweets that contain certain keywords.
+For each tweet, if you’re not the tweet author, it will mark the tweet as Liked and then retweet it.
+"""
 class FavRetweetListener(tweepy.StreamListener):
     def __init__(self, api):
         self.api = api
         self.me = api.me()
 
+    # If the tweet is a reply or not from me it will ignore it.
     def on_status(self, tweet):
         logger.info(f"Processing tweet id {tweet.id}")
         if tweet.in_reply_to_status_id is not None or tweet.user.id == self.me.id:
             return
+        """
+        This is like a maker, this is comming from tweepy favorited class.
+        If it did not favorited(Smiliary like) the tweet yet, it will favorited it if not it returns an error.
+        Try/Except is neccessary to handling possible Errors.
+        """
         if not tweet.favorited:
             try:
                 tweet.favorite()
             except Exception:
                 logger.error("Error on fav", exc_info=True)
+        """
+        A similar case like favorited class.
+        If it did not retweeted the tweet yet, it will retweeted it if not it returns an error.
+        Try/Except is neccessary to handling possible Errors.
+        """
         if not tweet.retweeted:
             try:
                 tweet.retweet()
             except Exception:
                 logger.error("Error on fav and retweet", exc_info=True)
 
+    # Logger for errors.
     def on_error(self, status):
         logger.error(status)
 
-
+"""
+The main uses a stream to filter tweets that contain the words of a list as keywords parametrs.
+Bellow we used ["Python", "Tweepy", "Hacktoberfest"]
+"""
 def main(keywords):
     api = create_api()
     tweets_listener = FavRetweetListener(api)
@@ -41,4 +61,4 @@ def main(keywords):
 
 
 if __name__ == "__main__":
-    main(["Python", "Tweepy"])
+    main(["Python", "Tweepy", "Hacktoberfest"])
--- tweetBot/followfollowers.py
+++ tweetBot/followfollowers.py
@@ -8,7 +8,11 @@ import time
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
-
+'''
+A lot of Twitter API endpoints use pagination to return their results.
+Tweepy cursors take away part of the complexity of working with paginated results.
+The Cursor object is iterable and takes care of fetching the various result pages transparently.
+'''
 def follow_followers(api):
     logger.info("Retrieving and following followers")
     for follower in tweepy.Cursor(api.followers).items():
@@ -16,7 +20,10 @@ def follow_followers(api):
             logger.info(f"Following {follower.name}")
             follower.follow()
 
-
+"""
+The Bot gets list of followers every minutes (adjustable within time.sleep param)
+and iterate through it to follow user who are not already followed.
+"""
 def main():
     api = create_api()
     while True:
