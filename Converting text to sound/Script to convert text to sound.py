9994e81948495b0390a53b2155723fd8fe97fcd8
Anustup Mukherjee <60361231+Anustup900@users.noreply.github.com>
2020-10-09 18:32:47 +0530

    Update Script to convert text to sound.py

--- Converting text to sound/Script to convert text to sound.py
+++ Converting text to sound/Script to convert text to sound.py
@@ -8,40 +8,17 @@ Created on Fri Oct  9 11:40:35 2020
 from tkinter import tkinter
 from gtts import gTTS
 from playsound import playsound
-
-
-
-################### Initialized window####################
-
 root = Tk()
 root.geometry('350x300')
 root.resizable(0,0)
 root.config(bg = 'ghost white')
 root.title('DataFlair - TEXT_TO_SPEECH')
-
-
-##heading
 Label(root, text = 'TEXT_TO_SPEECH' , font='arial 20 bold' , bg ='white smoke').pack()
 Label(root, text ='DataFlair' , font ='arial 15 bold', bg = 'white smoke').pack(side = BOTTOM)
-
-
-
-
-#label
 Label(root, text ='Enter Text', font ='arial 15 bold', bg ='white smoke').place(x=20,y=60)
-
-
-##text variable
 Msg = StringVar()
-
-
-#Entry
 entry_field = Entry(root,textvariable =Msg, width ='50')
 entry_field.place(x=20 , y=100)
-
-
-###################define function##############################
-
 def Text_to_speech():
     Message = entry_field.get()
     speech = gTTS(text = Message)
@@ -54,11 +31,8 @@ def Exit():
 def Reset():
     Msg.set("")
 
-#Button
 Button(root, text = "PLAY" , font = 'arial 15 bold', command = Text_to_speech, width =4).place(x=25, y=140)
 Button(root,text = 'EXIT',font = 'arial 15 bold' , command = Exit, bg = 'OrangeRed1').place(x=100,y=140)
 Button(root, text = 'RESET', font='arial 15 bold', command = Reset).place(x=175 , y =140)
 
-
-#infinite loop to run program
 root.mainloop()
