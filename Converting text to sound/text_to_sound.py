7150a4fe2465dba45edee7edfd3bb598ac771f14
Anustup Mukherjee <60361231+Anustup900@users.noreply.github.com>
2020-10-09 19:44:46 +0530

    Update text_to_sound.py

--- Converting text to sound/text_to_sound.py
+++ Converting text to sound/text_to_sound.py
@@ -1,15 +1,7 @@
 from gtts import gTTS
 from playsound import playsound
-text_val = 'All the best for your exam.'  
-
+text_val = 'All the best for your exam.'
 language = 'en'
-# Passing the text and language to the engine,
-# here we have assign slow=False. Which denotes  
-# the module that the transformed audio should
-# have a high speed
-obj = gTTS(text=text_val, lang=language, slow=False)  
-#Here we are saving the transformed audio in a mp3 file named
-# exam.mp3
-obj.save("exam.mp3")  
-# Play the exam.mp3 file
+obj = gTTS(text=text_val, lang=language, slow=False)
+obj.save("exam.mp3")
 playsound("exam.mp3")
