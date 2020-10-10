bf58f498d3a31eb0fd113750084be555a4c9fb9a
Ashkan Kamyab <ashkankamyab@gmail.com>
2020-10-10 16:17:24 +0200

    Updated README.md

--- tweet_bot/tweet-cli.py
+++ tweet_bot/tweet-cli.py
@@ -12,12 +12,15 @@ example:
                     tweet-cli "this is my tweet from tweet-cli"
 """
 
+
 def tweet_cli(api):
     if not len(sys.argv) == 2:
         print(usage)
     else:
         api.update_status(sys.argv[1])
 
+
 if __name__ == "__main__":
     api = create_api()
     tweet_cli(api)
+
