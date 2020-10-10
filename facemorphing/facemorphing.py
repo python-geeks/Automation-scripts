46d52bae89596bf847fb1c8f60bb609dc8e0b446
Anustup Mukherjee <60361231+Anustup900@users.noreply.github.com>
2020-10-10 18:19:34 +0530

    Update facemorphing.py

--- facemorphing/facemorphing.py
+++ facemorphing/facemorphing.py
@@ -23,9 +23,9 @@ def applyAffineTransform(src, srcTri, dstTri, size):
     # Given a pair of triangles, find the affine transform.
     warpMat = cv2.getAffineTransform(np.float32(srcTri), np.float32(dstTri))
     # Apply the Affine Transform just found to the src image
-    dst = cv2.warpAffine(src, warpMat, (size[0], size[1]), None
-                         ,flags=cv2.INTER_LINEAR
-                         ,borderMode=cv2.BORDER_REFLECT_101)
+    dst = cv2.warpAffine(src, warpMat, (size[0], size[1]), None,
+                         flags=cv2.INTER_LINEAR,
+                         borderMode=cv2.BORDER_REFLECT_101)
     return dst
 
 
