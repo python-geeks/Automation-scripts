4d755792185412d18c389bdcd2a1d173b49fa618
Jeel Pujara <jeelpujara2373@gmail.com>
2020-10-10 23:22:24 +0530

    Update main.py

--- text_from_image/main.py
+++ text_from_image/main.py
@@ -1,9 +1,9 @@
-import cv2
-import sys
 import pytesseract
 from PIL import Image
-pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
-text = pytesseract.image_to_string(Image.open(r'D:\\PersonalProjects\\temp\\Text from Image\\test.png'), lang='eng')
+path = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = path
+image = r'D:\\PersonalProjects\\temp\\Text from Image\\test.png'
+text = pytesseract.image_to_string(Image.open(image), lang='eng')
 file = open(r'D:\\PersonalProjects\\temp\\Text from Image\\output.txt', 'w')
 file.write(text)
 file.close()
