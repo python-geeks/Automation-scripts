a481c817b5e0ebe53cf25e059c292ce32ee15627
mateuszkowalke <mateusz.kowalke@gmail.com>
2020-10-10 20:11:33 +0200

    Add check for ping time.

--- speed_tester/speed_tester.py
+++ speed_tester/speed_tester.py
@@ -1,12 +1,27 @@
 import speedtest
+import subprocess
+import platform
+import re
+
+
+def ping_google():
+    try:
+        system = platform.system().lower()
+        option = "n" if system == "windows" else "c"
+        res = subprocess.check_output(
+            f"ping -{option} 10 google.com", shell=True, universal_newlines=True
+        )
+        if "unreachable" in res:
+            return False
+        else:
+            return res
+    except Exception:
+        return False
 
 
 def test_speed():
     units = ["bps", "Kbps", "Mbps", "Gbps"]
-    servers = []
     test = speedtest.Speedtest()
-    test.get_servers(servers)
-    test.get_best_server()
     download_speed = test.download()
     unit_index = 0
     while download_speed > 999:
@@ -19,6 +34,14 @@ def test_speed():
         upload_speed /= 1000
         unit_index += 1
     upload_units = units[unit_index]
+    ping = ping_google()
+    if ping:
+        times = re.findall("(?<=time=)\d+\.?\d*", ping)
+        times = [float(time) for time in times]
+        av_ping = sum(times) / len(times)
+        print(f"Average ping time for google.com is: {av_ping}")
+    else:
+        print("Can't connect to google.com")
     print(f"Download speed is: {round(download_speed, 3)}{download_units}")
     print(f"Upload speed is: {round(upload_speed, 3)}{upload_units}")
 
