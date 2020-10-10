2a55bcec7c397ffe9e65e85dedc619d600ede7f1
Saman Ebrahimnezhad <ebsa491@gmail.com>
2020-10-10 17:49:10 +0330

    Migration to sqlite3 and better performence

--- Password_Manager/password_manager.py
+++ Password_Manager/password_manager.py
@@ -4,14 +4,20 @@
 import argparse
 import sys
 import os
-from getpass import getpass
+import sqlite3
 import base64
+from getpass import getpass
 from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
 from Crypto import Random
 
 # The program data file.
-FILENAME = ".data.txt"
+FILENAME = ".pass.db"
+
+# Cookie(canary) value
+COOKIE = "===PASSWORDS==="
+
+# For coloring the outputs
 GREEN_COLOR = "\033[1;32m"
 RED_COLOR = "\033[1;31m"
 NO_COLOR = "\033[0m"
@@ -19,45 +25,86 @@ NO_COLOR = "\033[0m"
 
 def main():
 
-    if args.password == '' or args.password is None:
-        # User didn't enter the password
+    status, is_first = check_database()
 
-        if os.path.isfile(FILENAME):
-            # File exists (the user used the program before)
-            # Ask the password
-            # secret prompt for entering the password
-            password = getpass("Enter the password> ")
-            check_password(password)
-        else:
-            # File doesn't exist (it's the first time)
-            # Welcome
-            welcome()
-    else:
-        # We have the password here
-
-        if os.path.isfile(FILENAME):
-            # File exists (the user used the program before)
-            # Check the password
-            check_password(args.password)
-        else:
-            # File doesn't exist (it's the first time)
-            # Ignore the password and welcome
-            welcome()
-
-
-def welcome():
-    print("Welcome to my PASSWORD MANAGER!")
-    prompt()
-
-
-def prompt():
-    passwords = show()
-
-    if passwords == -1:
-        print(f"[{RED_COLOR}-{NO_COLOR}] Can't show the passwords...")
+    if status == -1:
+        print(f"[{RED_COLOR}-{NO_COLOR}] Error in database connection...")
         sys.exit(1)
 
-    print_passwords(passwords)
+    if args.password == '' or args.password is None:
+        # User didn't enter the password (argument)
+        if is_first:
+            user_password = getpass("Enter a new password for the program> ")
+            confirm = getpass("Confirm it> ")
+            if user_password != confirm:
+                print(f"[{RED_COLOR}-{NO_COLOR}] Didn't match...")
+                sys.exit(1)
+
+            if new(user_password, COOKIE, '-', 0) != 0:
+                print(f"[{RED_COLOR}-{NO_COLOR}] Error in saving...")
+                os.remove(FILENAME)
+                sys.exit(1)
+        else:
+            user_password = getpass("Enter your user password> ")
+
+            if check_password(user_password) != 1:
+                print(f"[{RED_COLOR}-{NO_COLOR}] Wrong password...")
+                sys.exit(1)
+    else:
+        # We have the password here
+        user_password = args.password
+
+        if check_password(user_password) != 1:
+            print(f"[{RED_COLOR}-{NO_COLOR}] Wrong password...")
+            sys.exit(1)
+
+    prompt(user_password)
+
+
+def check_database():
+    try:
+
+        is_first = False
+
+        if not os.path.isfile(FILENAME):
+            is_first = True
+
+        conn = sqlite3.connect(FILENAME)
+        cursor = conn.cursor()
+        cursor.execute(
+            """CREATE TABLE IF NOT EXISTS passwords (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                password TEXT NOT NULL
+                );"""
+        )
+        conn.commit()
+        conn.close()
+        return 0, is_first
+    except Exception:
+        return -1, False
+
+
+def check_password(user_password):
+    try:
+        conn = sqlite3.connect(FILENAME)
+        cursor = conn.cursor()
+        rows = cursor.execute(
+            "SELECT * FROM passwords WHERE name=?;",
+            encrypt(
+                str(user_password).encode('utf-8'),
+                COOKIE.encode('utf-8')
+            )
+        ).fetchall()
+        if rows.count > 0:
+            return 1
+        else:
+            return 0
+    except Exception:
+        return -1
+
+
+def prompt(user_password):
 
     print("Commands: [new] [delete] [show] [exit]")
     cmd = input("Enter your command> ")
@@ -65,16 +112,16 @@ def prompt():
     if cmd.lower() == 'new':
 
         name = input("Where did you use this password for> ")
-        password = input("Enter the new password> ")
+        password = input("Enter the new password to save> ")
 
-        if new(passwords.count, name, password) == 0:
+        if new(user_password, name, password) == 0:
             print(f"[{GREEN_COLOR}+{NO_COLOR}] Successfully added...")
-            prompt()
+            prompt(user_password)
         else:
             print(
                 f"[{RED_COLOR}-{NO_COLOR}] Error while writing the password..."
             )
-            prompt()
+            prompt(user_password)
 
     elif cmd.lower() == 'delete':
 
@@ -84,75 +131,120 @@ def prompt():
         if confirm.lower() == 'y':
             if delete(id_num) == 0:
                 print(f"[{GREEN_COLOR}+{NO_COLOR}] Successfully deleted...")
+            else:
+                print(f"[{RED_COLOR}-{NO_COLOR}] Error in deleting...")
 
-        prompt()
+        prompt(user_password)
 
     elif cmd.lower() == 'show':
 
-        passwords = show()
-        print_passwords(passwords)
-        prompt()
+        result = select_data(user_password)
+
+        if result == -1:
+            print(f"[{RED_COLOR}-{NO_COLOR}] Error in selecting the data...")
+        else:
+            show_data(result)
+        prompt(user_password)
 
     elif cmd.lower() == 'exit':
         sys.exit(0)
     else:
         print("Command not found...")
-        prompt()
+        prompt(user_password)
 
 
-def new(id_num, name, password):
+def new(user_password, name, password, id_num=-1):
     try:
-        with open(FILENAME, 'a') as data_file:
-            name = str(name).replace('\n', '')
-            password = str(password).replace('\n', '')
-            data_file.write(f"{id_num}\t{name}\t{password}")
-            return 0
+        conn = sqlite3.connect(FILENAME)
+        cursor = conn.cursor()
+        if id_num == -1:
+            cursor.execute(
+                """INSERT INTO passwords(name, password) VALUES (
+                        ?,?
+                    );""",
+                [
+                    encrypt(
+                        str(user_password).encode('utf-8'),
+                        str(name).encode('utf-8')
+                    ),
+                    encrypt(
+                        str(user_password).encode('utf-8'),
+                        str(password).encode('utf-8')
+                    )
+                ]
+            )
+        else:
+            cursor.execute(
+                """INSERT INTO passwords(id, name, password) VALUES (
+                        ?,?,?
+                    );""",
+                [
+                    id_num,
+                    encrypt(
+                        str(user_password).encode('utf-8'),
+                        str(name).encode('utf-8')
+                    ),
+                    encrypt(
+                        str(user_password).encode('utf-8'),
+                        str(password).encode('utf-8')
+                    )
+                ]
+            )
+        conn.commit()
+        conn.close()
+        return 0
     except Exception:
         return -1
 
 
 def delete(id_num):
     try:
-        with open(FILENAME, 'r') as data_file:
-            lines = data_file.readlines()
-        with open(FILENAME, 'w') as data_file:
-            for line in lines:
-                if line.strip("\n").split("\t")[0] != str(id_num):
-                    data_file.write(line)
-            return 0
+        conn = sqlite3.connect(FILENAME)
+        cursor = conn.cursor()
+        cursor.execute(
+            "DELETE FROM passwords WHERE id = ? AND name != ?;",
+            [id_num, COOKIE]
+        )
+        conn.commit()
+        conn.close()
+        return 0
     except Exception:
         return -1
 
 
-def show():
+def select_data(user_password):
     try:
-        with open(FILENAME, 'r') as data_file:
-            # *[1:] => Ignore the first line ('===PASSWORDS===')
-            return data_file.readlines()[1:]
+        conn = sqlite3.connect(FILENAME)
+        cursor = conn.cursor()
+        passwords = cursor.execute("SELECT * FROM passwords;").fetchall()
+        conn.close()
+
+        result = []
+
+        for (id_num, name, password) in passwords:
+            result.append(
+                (
+                    id_num,
+                    decrypt(
+                        str(user_password).encode('utf-8'), name
+                    ).decode('utf-8'),
+                    decrypt(
+                        str(user_password).encode('utf-8'), password
+                    ).decode('utf-8')
+                )
+            )
+        return result
     except Exception:
         return -1
 
 
-def print_passwords(passwords):
-    for password in passwords:
-        properties = password.split('\t')
-        print(f"{properties[0]} => {properties[1]}, {properties[2]}")
-
-
-def check_password(password):
-    try:
-        with open(FILENAME, 'r') as data_file:
-            data = decrypt(password, data_file.read())
-            if data[:15] == '===PASSWORDS===':
-                # Password is correct
-                prompt()
-            else:
-                # Password is wrong
-                input_password = getpass("Wrong, Enter again> ")
-                check_password(input_password)
-    except Exception:
-        print(f"[{RED_COLOR}-{NO_COLOR}] ERROR...")
-        sys.exit(1)
+def show_data(result):
+    if result.count == 0:
+        print(" ====== Nothing ====== ")
+    else:
+        for (id_num, name, password) in result:
+            if name != COOKIE:
+                print(f"{id_num} => [{name}] {password}")
 
 
 def encrypt(key, source, encode=True):
