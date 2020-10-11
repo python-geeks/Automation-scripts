6ef994814e6947b36fa96194b81dcfc17f79c38d
Ashkan Kamyab <ashkankamyab@gmail.com>
2020-10-11 02:36:58 +0200

    Corrected linting

--- wake_on_discount/amazon_price.py
+++ wake_on_discount/amazon_price.py
@@ -28,7 +28,7 @@ def get_price(URL):
         'cache-control': 'no-cache',
         'dnt': '1',
         'upgrade-insecure-requests': '1',
-        'user-agent': '''Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36
+        'user-agent': '''Mozilla/5.0 (X11; Chrome x86_64 8172.45.0) AppleWebKit/537.36
         (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36''',
         'accept': '''text/html,application/xhtml+xml,application/xml;q=0.9,image/
         webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9''',
