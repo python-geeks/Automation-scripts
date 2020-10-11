fa9c6d457e35a2ec2d19fc5d59cf47b270abe3da
TheFenrisLycaon <thefenrislycaon@gmail.com>
2020-10-11 18:26:04 +0530

    Linting

--- file_arranger/file_arranger.py
+++ file_arranger/file_arranger.py
@@ -1,29 +1,6 @@
 import os
 from pathlib import Path
 
-DIRECTORIES = {
-
-    "Webpages": [".html5", ".html", ".htm", ".xhtml", ".aspx", ".php"],
-
-    "Images": [".jpg", ".jpeg", ".png", ".tiff", ".gif", ".bmp", ".bpg", ".svg",
-                ".heif", ".psd"],
-
-    "Videos": [".avi", ".mp4", ".flv", ".mkv", ".wmv", ".mov", ".webm", ".vob",
-                ".3gp", ".mpeg", ".mpg", ".qt"],
-
-    "Docs": [".pdf", ".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx", ".epub",
-              ".opus", ".txt", ".in", ".out", ".xml"],
-
-    "Archive" : [".rar", ".zip", ".7z"],
-
-    "Audio" : [".mp3", ".aac", ".ogg", ".m4a", ".wav", ".aa", ".dvf", ".m4b",
-               ".m4p", ".msv", ".oga", ".raw", ".vox"]
-
-}
-
-FILE_FORMATS = {file_format: directory
-                for directory, file_formats in DIRECTORIES.items()
-                for file_format in file_formats}
 
 def organize_junk():
     for entry in os.scandir():
@@ -37,17 +14,43 @@ def organize_junk():
             directory_path = Path(FILE_FORMATS[file_format])
             directory_path.mkdir(exist_ok=True)
             file_path.rename(directory_path.joinpath(file_path))
-            
+
         else:
             directory_path = Path('Others')
             directory_path.mkdir(exist_ok=True)
             file_path.rename(directory_path.joinpath(file_path))
-        
+
         for dir in os.scandir():
             try:
                 os.rmdir(dir)
             except:
                 pass
+
+
+DIRECTORIES = {
+
+    "Webpages": [".html5", ".html", ".htm", ".xhtml", ".aspx", ".php"],
+
+    "Images": [".jpg", ".jpeg", ".png", ".tiff", ".gif", ".bmp", ".bpg", ".svg",
+               ".heif", ".psd"],
+
+    "Videos": [".avi", ".mp4", ".flv", ".mkv", ".wmv", ".mov", ".webm", ".vob",
+               ".3gp", ".mpeg", ".mpg", ".qt"],
+
+    "Docs": [".pdf", ".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx", ".epub",
+             ".opus", ".txt", ".in", ".out", ".xml"],
+
+    "Archive": [".rar", ".zip", ".7z"],
+
+    "Audio": [".mp3", ".aac", ".ogg", ".m4a", ".wav", ".aa", ".dvf", ".m4b",
+              ".m4p", ".msv", ".oga", ".raw", ".vox"]
+
+}
+
+FILE_FORMATS = {file_format: directory
+                for directory, file_formats in DIRECTORIES.items()
+                for file_format in file_formats}
+
 
 if __name__ == "__main__":
     organize_junk()
