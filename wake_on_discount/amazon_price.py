a31264ecec664a773bff3d881ec8b7ded99a6e18
Ashkan Kamyab <ashkankamyab@gmail.com>
2020-10-11 02:15:32 +0200

    Linting correction

--- wake_on_discount/amazon_price.py
+++ wake_on_discount/amazon_price.py
@@ -6,11 +6,13 @@ from twilio.rest import Client
 import time
 
 """
-Scrapper: get_price() digs amazon URL and exracts the ProductID of it. 
-Product ID is the very most unique part of amazon store.  
+Scrapper: get_price() digs amazon URL and exracts the ProductID of it.
+Product ID is the very most unique part of amazon store. 
 This helps to use the online portal camelcamelcamel.com.
 This approache makes this script persistance.
 """
+
+
 def get_price(URL):
     country_prefix = ""
     if "amazon.de" in URL:
@@ -26,8 +28,10 @@ def get_price(URL):
         'cache-control': 'no-cache',
         'dnt': '1',
         'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'user-agent': '''Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36
+                                             (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36''',
+        'accept': '''text/html,application/xhtml+xml,application/xml;q=0.9,image/
+                            webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9''',
         'sec-fetch-site': 'none',
         'sec-fetch-mode': 'navigate',
         'sec-fetch-dest': 'document',
@@ -44,26 +48,29 @@ def get_price(URL):
     '''
     return (float(current))
 
+
 """
 Simple Logic: Is the Product's price bellower I want, then inbox me or text me.
 """
+
 
 def check_price(URL, desired_price):
     if isinstance(desired_price) is False:
         raise TypeError('Desired Price should be Int or Double decimal')
     else:
-        if(desired_price < get_price(getenv("URL"))):
+        current_price = (get_price(getenv("URL")))
+        if(desired_price < current_price):
             # TODO: Sends email
             pass
-        if (desired_price >= get_price(getenv("URL"))):
+        if (desired_price >= current_price):
                     Client(getenv('ACCOUNT_SID'), getenv('AUTH_TOKEN')).messages.create(
-                    body='Current price for {} is {i}. Your deisred price is'.format(desired_price. get_price(URL)),
-                    from_='+twilio_number',
-                    to='+user_phone_number'
+                        body='Current price for {} is {}. Your deisred price is'.format(desired_price. current_price),
+                        from_='+twilio_number',
+                        to='+user_phone_number'
                 )
 
 
 if __name__ == "__main__":
     while True:
         check_price(200, getenv("URL"))
-        time.sleep((60 * 60) * 12) 
+        time.sleep((60 * 60) * 12)
