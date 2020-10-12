471b6626ea26bf0e7d2dc930225b714139f673b7
Hardik <bagadahardik2000@gmail.com>
2020-10-12 21:41:17 +0530

    feat : add watermark pdf script

---
+++ add_watermark_pdf/README.md
@@ -0,0 +1,15 @@
+### Watermark PDFs
+- add watermark pdf to input pdf.
+
+### Building & Running Step
+
+-  ```git clone <repo_url>```
+-  ```cd add_watermark_pdf``` 
+-   Here, demo pdfs are given so replace *watermark.pdf* and *Demo.pdf*.
+-  ```pip install -r requirenments.txt``` or ```pip install PyPDF2```
+-  ```python script.py``` 
+
+Your Output PDF is ready!! :smiley:
+
+*Note : PDFs are only for demo purpose.*
+*Author : Hardik Bagada*
---
+++ add_watermark_pdf/script.py
@@ -0,0 +1,24 @@
+"""
+@hardik
+@2020
+@add_watermark to pdf
+@py code
+"""
+import PyPDF2
+
+inputFile = open('Demo.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(inputFile)
+inputFirstPage = pdfReader.getPage(0)
+
+pdfWatermarkReader = PyPDF2.PdfFileReader(open('watermark.pdf', 'rb'))
+inputFirstPage.mergePage(pdfWatermarkReader.getPage(0))
+pdfWriter = PyPDF2.PdfFileWriter()
+pdfWriter.addPage(inputFirstPage)
+
+for pageNum in range(1, pdfReader.numPages):
+    pageObj = pdfReader.getPage(pageNum)
+    pdfWriter.addPage(pageObj)
+outputFile = open('output.pdf', 'wb') #outputfile name
+pdfWriter.write(outputFile)
+inputFile.close()
+outputFile.close()
