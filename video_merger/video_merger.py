b391a2d6bea02b7ca4714994a636552b031294e5
DragonGhost7 <darkiridiumghost@gmail.com>
2020-10-12 15:19:58 -0400

    fixed flake8 standards

--- video_merger/video_merger.py
+++ video_merger/video_merger.py
@@ -5,9 +5,9 @@ import ffmpeg
 if __name__ == '__main__':
     print(sys.argv)
     print(len(sys.argv))
-    print(str(sys.argv[len(sys.argv)-1]))
+    print(str(sys.argv[len(sys.argv) - 1]))
     list = open("list.txt", "w")
-    for i in range(1, len(sys.argv)-1):
+    for i in range(1, len(sys.argv) - 1):
         list.write("file {}\n".format(sys.argv[i]))
     list.close()
-    ffmpeg.input("list.txt", format='concat', safe=0).output(sys.argv[len(sys.argv)-1], c='copy').run()
+    ffmpeg.input("list.txt", format='concat', safe=0).output(sys.argv[len(sys.argv) - 1], c='copy').run()
