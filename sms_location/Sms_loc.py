227cc9bd9deb132d09f5fe8d0a896fdd1e15f4d0
Akash Dwivedi <akashdwivedi563@gmail.com>
2020-10-13 21:24:28 +0530

    Update Sms_loc.py
    lint in smsloc

--- sms_location/Sms_loc.py
+++ sms_location/Sms_loc.py
@@ -6,20 +6,18 @@ ipadd = ip_request.json()['ip']
 url = 'https://get.geojs.io/v1/ip/geo/' + ipadd + '.json'
 geo_request = requests.get(url)
 geo_data = geo_request.json()
-# print(geo_data)
 msg = f"latitude: {geo_data['latitude']} longitude : {geo_data['longitude']} city : {geo_data['city']}"
-# print(msg)
 url1 = "https://www.fast2sms.com/dev/bulk"
 query = {"authorization" : "your api key ",
-       "sender_id" : "FSTSMS",
-       "message" : msg,
-       "language" : "english",
-       "route" : "p",
-       "numbers" : no
-       }
+         "sender_id" : "FSTSMS",
+         "message" : msg,
+         "language" : "english",
+         "route" : "p",
+         "numbers" : no
+        }
 
 headers = {
     'cache-control' : "no-cache"
 }
-response = requests.request("GET" , url1 , headers = headers , params = query)
+response = requests.request("GET" , url1, headers = headers, params = query)
 print(response.text)
