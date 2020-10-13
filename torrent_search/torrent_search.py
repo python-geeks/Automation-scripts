4528c1e1bd1b586a30c143b51a6061827b6797ec
hdoerner <h.doerner@bitexpert.de>
2020-10-13 08:06:39 +0200

    Add script to search for torrents

---
+++ torrent_search/torrent_search.py
@@ -0,0 +1,28 @@
+import os
+import requests
+import json
+
+urlList = [
+    'https://thepiratebay.org',
+    'https://1337x.to/search' # /{query}/{page}/
+]
+
+
+def send_query(url):
+    pass
+
+
+def parse_thepiratebay():
+    pass
+
+
+def parse_1337to():
+    pass
+
+
+def run(debug=False):
+    pass
+    
+
+if __name__ == "__main__":
+    run(True)
