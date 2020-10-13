be6c043d310e987499a6d8869d45b2f276237857
Ferdinand Hahmann <git@fhahmann.de>
2020-10-13 08:55:14 +0200

    Format code to pylint

--- text_summarize/text_summarize.py
+++ text_summarize/text_summarize.py
@@ -13,20 +13,25 @@ def text_summarize(text, **kwargs):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Summarize the given text')
-    parser.add_argument('-t', '--text',  help="Text to summarize", type=str)
-    parser.add_argument('-f', '--file', help="Filename to read text from", type=str)
+    parser.add_argument('-t', '--text', help="Text to summarize",
+                        type=str)
+    parser.add_argument('-f', '--file', help="Filename to read text from",
+                        type=str)
     parser.add_argument('-r', '--ratio',
-                        help="Given the ratio of the summarized text (default: 0.2)",
+                        help="Given the ratio of the summarized text "
+                             "(default: 0.2)",
                         type=float, default=0.2)
     parser.add_argument('-o', '--output',
-                        help="Given the path to an output file. Otherwise stdout will be used",
+                        help="Given the path to an output file. "
+                             "Otherwise stdout will be used",
                         type=str, default=None)
     args = parser.parse_args()
 
     if not (args.text or args.file):
         parser.error("Either --text or --file is required")
     if args.text and args.file:
-        parser.error("The arguments --text and --file are not allowed together")
+        parser.error("The arguments --text and --file are not "
+                     "allowed together")
 
     if args.file:
         with open(args.file, 'r') as infile:
