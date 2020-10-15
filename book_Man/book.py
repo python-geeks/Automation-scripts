d85edb6bfd1ea7d898476e0f21688c87a0ef8ff2
Kiran MHM <46129076+GoluffyxD@users.noreply.github.com>
2020-10-15 15:01:09 +0530

    Edited , issues

--- book_Man/book.py
+++ book_Man/book.py
@@ -6,30 +6,30 @@ import sqlite3
 # c = conn.cursor()
 
 # c.execute(""" CREATE TABLE books (
-#         name text,
-#         path text primary key,
-#         tags text,
+#         name text, 
+#         path text primary key,  
+#         tags text,  
 #         notes text
 # )   """)
 
-# c.execute("INSERT INTO books VALUES ('test1','path1','notes1','fiction')")
+# c.execute("INSERT INTO books VALUES ('test1', 'path1', 'notes1', 'fiction')")
 # conn.commit()
 
-def add_book(conn,book):
+def add_book(conn, book):
     c=conn.cursor()
-    c.execute("INSERT INTO books VALUES (?,?,?,?)",(book.name,book.path,book.notes,book.tags))
+    c.execute("INSERT INTO books VALUES (?, ?, ?, ?)", (book.name, book.path, book.notes, book.tags))
     conn.commit()
 
 
-def delete_book(conn,path):
+def delete_book(conn, path):
     c=conn.cursor()
-    c.execute("DELETE FROM books WHERE path = ? ",(path,))
+    c.execute("DELETE FROM books WHERE path = ? ", (path, ))
     conn.commit()
 
-def edit_book(conn,prop,value,name):
+def edit_book(conn, prop, value, name):
     c=conn.cursor()
-    if prop in ['name','notes','tags','path']:
-        c.execute(f"UPDATE books SET {prop} = ? WHERE path = ?",(value,name))
+    if prop in ['name', 'notes', 'tags', 'path']:
+        c.execute(f"UPDATE books SET {prop} = ? WHERE path = ?", (value, name))
         conn.commit()
 
 def show_books(conn):
@@ -38,13 +38,13 @@ def show_books(conn):
     finresults=[]
     results= c.fetchall()
     for i in results:
-        finresults.append([i[0],i[1],i[2].split(','),i[3]])
+        finresults.append([i[0], i[1], i[2].split(', '), i[3]])
     return finresults
 
-def search_book(conn,prop,value):
+def search_book(conn, prop, value):
     c=conn.cursor()
-    if prop in ['name','notes','tags','path']:
-        c.execute(f"SELECT * FROM books where {prop} = ?",(value,))
+    if prop in ['name', 'notes', 'tags', 'path']:
+        c.execute(f"SELECT * FROM books where {prop} = ?", (value, ))
         conn.commit()
 
 # print("Welcome to BookMan")
@@ -52,11 +52,11 @@ def search_book(conn,prop,value):
 #     print("1.Add New Book\n2.Edit a Book\n3.Display all books\n4.Delete a book\n5.Exit")
 #     n=int(input())
 #     if n==1:
-#         name,path,notes,tags= input("Enter Book Details: ").split()
-#         add_book(Book(name,path,notes,tags))
+#         name, path, notes, tags= input("Enter Book Details: ").split()
+#         add_book(Book(name, path, notes, tags))
 #     elif n==2:
-#         prop,value,name = input("Enter property, new value and book name: ").split()
-#         edit_book(prop,value,name)
+#         prop, value, name = input("Enter property,  new value and book name: ").split()
+#         edit_book(prop, value, name)
 #     elif n==3:
 #         show_books()
 #     elif n==4:
