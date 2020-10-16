7d0a6482d53de6238686279f2c391e9493392fd9
Shushant Rishav <54536673+Rishu20@users.noreply.github.com>
2020-10-16 14:58:42 +0530

    Added pdf to image
    a simple script that can be used to convert multiple pdfs to png

---
+++ Pdf_to_image/main_code.py
@@ -0,0 +1,35 @@
+import os, sys
+from PIL import Image
+from PyPDF2 import PdfFileMerger
+from fnmatch import fnmatch
+from pdf2image import convert_from_path
+from pdf2image.exceptions import (
+ PDFInfoNotInstalledError,
+ PDFPageCountError,
+ PDFSyntaxError
+)
+sys.path.insert(1, 'B:/Production_Programs/Github/Automation-scripts/Pdf_to_image/sample_files')
+
+fileName = "myfile.pdf"
+if os.path.exists(fileName):
+    os.remove(fileName)
+
+##merge pdfs into one
+x = [a for a in os.listdir() if a.endswith(".pdf")]
+merger = PdfFileMerger()
+for pdf in x:
+    merger.append(open(pdf, 'rb'))
+
+with open(fileName, "wb") as fout:
+    merger.write(fout)
+
+##convert merged pdf to png files
+images = convert_from_path(fileName)
+
+for i, image in enumerate(images):
+    fname = 'image'+str(i)+'.png'
+    image.save(fname, "PNG")
+
+print("all file converted")
+
+
