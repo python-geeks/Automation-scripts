ebe931ca837e6a779aada00eb98489a0cf7726d2
m4Dummies <m4dummies@gmail.com>
2020-10-19 23:22:36 +0530

    Fixed lint error

--- google-meet-joiner/main.py
+++ google-meet-joiner/main.py
@@ -4,52 +4,50 @@ import schedule
 import pyautogui
 
 
-url1 = "https://meet.google.com/"
-url2 = "https://www.google.com/"
+url1 = "https://meet.google.com"
+url2 = "https://www.google.com"
 
 
 def class1():
-	webbrowser.open(url1)
-	time.sleep(3)
-	print("Joining Class")
-	time.sleep(2)
-	pyautogui.hotkey('command', 'd')
-	print("turned off camera")
-	time.sleep(2)
-	pyautogui.hotkey('command', 'e')
-	print("turned off mic")
-	time.sleep(3)
+    webbrowser.open(url1)
+    time.sleep(3)
+    print("Joining Class")
+    time.sleep(2)
+    pyautogui.hotkey('command', 'd')
+    print("Turned off camera")
+    pyautogui.hotkey('command', 'e')
+    print("Turned off mic")
+    time.sleep(3)
 
-	for i in range(4):
-		pyautogui.press('tab')
+    for i in range(4):
+        pyautogui.press('tab')
 
-	time.sleep(3)
-	pyautogui.press('enter')
+    time.sleep(3)
+    pyautogui.press('enter')
 
 
-def class2():
-	webbrowser.open(url2)
-	time.sleep(3)
-	print("Joining Class")
-	time.sleep(2)
-	pyautogui.hotkey('command', 'd')
-	print("turned off camera")
-	time.sleep(2)
-	pyautogui.hotkey('command', 'e')
-	print("turned off mic")
-	time.sleep(3)
+def class1():
+    webbrowser.open(url2)
+    time.sleep(3)
+    print("Joining Class")
+    time.sleep(2)
+    pyautogui.hotkey('command', 'd')
+    print("Turned off camera")
+    pyautogui.hotkey('command', 'e')
+    print("Turned off mic")
+    time.sleep(3)
 
-	for i in range(4):
-		pyautogui.press('tab')
+    for i in range(4):
+        pyautogui.press('tab')
 
-	time.sleep(3)
-	pyautogui.press('enter')
+    time.sleep(3)
+    pyautogui.press('enter')
 
 
 if __name__ == "__main__":
-	schedule.every().day.at("16:37").do(class1)
-	schedule.every().day.at("14:00").do(class2)
+    schedule.every().day.at("16:00").do(class1)
+    schedule.every().day.at("06:00").do(class2)
 
-	while True:
-		schedule.run_pending()  # check if we need to run anything
-		time.sleep(10)  # wait 10 seconds before checking each time again
+    while True:
+        schedule.run_pending() #check if we need to run anything
+        time.sleep(10) # wait 10 second before checking again
