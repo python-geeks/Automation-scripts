c214d078b4117502e48ca2734039a19b1773ea09
m4Dummies <m4dummies@gmail.com>
2020-10-19 16:44:35 +0530

    Google meet joiner

---
+++ google-meet/README.md
@@ -0,0 +1,29 @@
+# Google Meet Automation
+
+This python script will help you to join daily repetitve classses easily. 
+
+
+### Getting Started
+
+- Firstly clone the project to the desired location, and locate it in the terminal. You would have to download the required modules, you can do this by doing: 
+  `pip install requirements.txt` or `pip3 install requirements.txt`
+  
+- You can now modify the script slightly according to your schedule: 
+	1) If you are using chrome as you browser please replace the `for i in range(4)` as show below:
+		image here
+     
+		If you are using safari do not change the script.
+    
+	2) You can now replace the links with your meet links here: 
+		Image here
+      
+	3) You can replace the timings here (in military time): 
+		Image here
+
+	4) Lastly you can replace 'command' with 'ctrl' if you are on windows.
+
+- This script will let you run two meetings that you have on a daily bases.
+
+
+- Please note: The following program supports python3
+
---
+++ google-meet/main.py
@@ -0,0 +1,52 @@
+import time
+import webbrowser
+import schedule
+import pyautogui
+
+url1 = "https://meet.google.com/uzi-pban-npm"
+url2 = "https://www.google.com/?client=safari"
+
+def class1():
+	webbrowser.open(url1)
+	time.sleep(3)
+	print("Joining Class")
+	time.sleep(2)
+	pyautogui.hotkey('command', 'd')
+	print("turned off camera")
+	time.sleep(2)
+	pyautogui.hotkey('command', 'e')
+	print("turned off mic")
+	time.sleep(3)
+
+	for i in range(4):
+		pyautogui.press('tab')
+
+	time.sleep(3)
+	pyautogui.press('enter')
+
+def class2():
+	webbrowser.open(url2)
+	time.sleep(3)
+	print("Joining Class")
+	time.sleep(2)
+	pyautogui.hotkey('command', 'd')
+	print("turned off camera")
+	time.sleep(2)
+	pyautogui.hotkey('command', 'e')
+	print("turned off mic")
+	time.sleep(3)
+
+	for i in range(4):
+		pyautogui.press('tab')
+
+	time.sleep(3)
+	pyautogui.press('enter')
+
+
+if __name__ == "__main__":
+    schedule.every().day.at("16:37").do(class1)
+    schedule.every().day.at("14:00").do(class2)
+
+    while True:
+        schedule.run_pending()  # check if we need to run anything
+        time.sleep(10)  # wait 10 seconds before checking each time again
