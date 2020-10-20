27f7075d0b0ef10a812decaff7efa385dd590ada
m4Dummies <m4dummies@gmail.com>
2020-10-20 17:12:02 +0530

    Lint errors

--- watermark-maker/main.py
+++ watermark-maker/main.py
@@ -14,15 +14,12 @@ with open(pdf_file, "rb") as input_file, \
     watermark_pdf = PdfFileReader(watermark_file)
     watermark_page = watermark_pdf.getPage(0)
 
-
     output = PdfFileWriter()
-
 
     for i in range(input_pdf.getNumPages()):
         pdf_page = input_pdf.getPage(i)
         pdf_page.mergePage(watermark_page)
         output.addPage(pdf_page)
-
 
     with open(merged, "wb") as merged_file:
         output.write(merged_file)
