00e1530163d113116cb4deb61d6c8184dbf6b713
m4Dummies <m4dummies@gmail.com>
2020-10-20 21:37:45 +0530

    Lint errors

--- watermark-maker/main.py
+++ watermark-maker/main.py
@@ -6,7 +6,7 @@ watermark = input("Watermark file: ")
 merged = "finalDraft.pdf"
 
 with open(pdf_file, "rb") as input_file,\
- open(watermark, "rb") as watermark_file:
+        open(watermark, "rb") as watermark_file:
     input_pdf = PdfFileReader(input_file)
     watermark_pdf = PdfFileReader(watermark_file)
     watermark_page = watermark_pdf.getPage(0)
