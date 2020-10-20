787ebf6454b6008d04fe041edfca78094063f3f6
m4Dummies <m4dummies@gmail.com>
2020-10-20 08:25:03 +0530

    Added change

--- google-meet-joiner/main.py
+++ google-meet-joiner/main.py
@@ -8,7 +8,7 @@ url1 = "https://meet.google.com"
 url2 = "https://www.google.com"
 
 
-def class1():
+def attend_class(url):
     webbrowser.open(url1)
     time.sleep(3)
     print("Joining Class")
@@ -26,27 +26,9 @@ def class1():
     pyautogui.press('enter')
 
 
-def class2():
-    webbrowser.open(url2)
-    time.sleep(3)
-    print("Joining Class")
-    time.sleep(2)
-    pyautogui.hotkey('command', 'd')
-    print("Turned off camera")
-    pyautogui.hotkey('command', 'e')
-    print("Turned off mic")
-    time.sleep(3)
-
-    for i in range(4):
-        pyautogui.press('tab')
-
-    time.sleep(3)
-    pyautogui.press('enter')
-
-
 if __name__ == "__main__":
-    schedule.every().day.at("16:00").do(class1)
-    schedule.every().day.at("06:00").do(class2)
+    schedule.every().day.at("16:00").do(attend_class(url1))
+    schedule.every().day.at("06:00").do(attend_class(url2))
 
     while True:
         schedule.run_pending()  # check if we need to run anything
