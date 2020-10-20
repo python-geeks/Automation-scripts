40c8b09f2807476285282ca2016c69102e4133c7
m4Dummies <m4dummies@gmail.com>
2020-10-20 17:00:41 +0530

    WaterMark maker

---
+++ watermark-maker/README.md
@@ -0,0 +1,9 @@
+# Watermark Maker
+
+### Getting started
+ - This python script can add a watermark to any pdf. However you need to have the watermark image or text in a .pdf file.
+ - To begin with you will have to download all the required modules for the script to run. You can do that by typing: `pip install -r requirements.txt` or `pip3 install -r requirements.txt`
+ -  After executing the program it will ask you for the name of the pdf file you want to add the watermakr to and the name of the pdf file that you want to use as a watermark. 
+ -  The final pdf would be saved by the name finalDraft.pdf 
+ -  The script will add watermark to all the pages that the pdf contains.
+
---
+++ watermark-maker/main.py
@@ -0,0 +1,23 @@
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+pdf_file = input("Please enter the name of the pdf file \
+you wish to add a watermark to: ")
+watermark = input("Please enter the name of the pdf file\
+that you want to use as a watermark: ")
+merged = "finalDraft.pdf"
+
+with open(pdf_file, "rb") as input_file, \
+ open(watermark, "rb") as watermark_file:
+    input_pdf = PdfFileReader(input_file)
+    watermark_pdf = PdfFileReader(watermark_file)
+    watermark_page = watermark_pdf.getPage(0)
+
+    output = PdfFileWriter()
+
+    for i in range(input_pdf.getNumPages()):
+        pdf_page = input_pdf.getPage(i)
+        pdf_page.mergePage(watermark_page)
+        output.addPage(pdf_page)
+
+    with open(merged, "wb") as merged_file:
+        output.write(merged_file)
