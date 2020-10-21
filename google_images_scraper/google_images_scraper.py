e76377d779ed1b7001f91275e2b0d7b938f52737
sripaad <sripaad751@gmail.com>
2020-10-21 21:26:47 +0530

    fixed trailing errors

--- google_images_scraper/google_images_scraper.py
+++ google_images_scraper/google_images_scraper.py
@@ -4,68 +4,61 @@ import urllib.request
 import time
 import argparse
 import os
-import driver_downloader
+
+import geckodriver_autoinstaller
 
 
-def image_search_scrape():
-    if os.path.isfile("/usr/bin/geckodriver"):
+def driver_download():
+    # Check if the current version of geckodriver exists
+    # if it doesn't exist, download it automatically,
+    # then add geckodriver to PATH.
+    geckodriver_autoinstaller.install()
+
+
+def google_images_scraper():
+    if os.path.isfile("$GECKO_PATH"):  # /usr/bin/geckodriver
         pass
     else:
-        driver_downloader.driver_download()
+        driver_download()
     # taking user arguments
     parser = argparse.ArgumentParser(description="Scrape Google images")
     parser.add_argument(
-        "-s", "--search", default="", type=str, help="search term"
+        "-s", "--search", default="mangoes", type=str, help="search term"
     )
     parser.add_argument(
-        "-d", "--directory", default="/images", type=str, help="save directory"
-    )
-    parser.add_argument(
-        "-iC",
-        "image_count",
-        default="100",
-        type=int,
-        help="Number of images to dowmload",
+        "-d", "--directory", default="images/", type=str, help="save directory"
     )
     args = parser.parse_args()
     query = args.search
     savePath = args.directory
-    numImages = args.image_count
     print("Search Term is " + query)
-    print("Images will be saved to this " + savePath + " directory")
+    print("Images will be saved to " + savePath + " directory")
     site = "https://www.google.com/search?tbm=isch&q=" + query
-    # providing driver path (usually in /usr/bin/geckodriver) if not
-    # then current dir (check line 13)
-    driver = webdriver.Firefox(executable_path=".")
+    # providing driver path
+    driver = webdriver.Firefox(executable_path="$GECKO_PATH")
     # passing site url
     driver.get(site)
     """
-    the below scrapes only the first instance of the search, no scrolling.
+    if you just want to download 10-15 images then skip the while loop and
+    just write
+    driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
+    below while loop scrolls the webpage 5 times(if available,
+    also scrolling beyond that the results get a bit diluted)
+    (will be able to retrieve around 250-300 images)
     """
-    if numImages <= 15:
+    i = 0
+    while i < 5:
+        # for scrolling page
         driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
 
-    else:
-        """
-        below while loop scrolls the webpage 5 times(if available, also
-        scrolling beyond that the results get a bit diluted)
-        (will be able to retrieve around 250-300 images)
-        """
-        i = 0
-        while i < 5:
-            # for scrolling page
-            driver.execute_script(
-                "window.scrollBy(0,document.body.scrollHeight)"
-            )
-
         try:
-            # for clicking show more results
-            # button
+            # for clicking show more results button
             driver.find_element_by_xpath(
                 "/html/body/div[2]/c-wiz/div[3]/div[1]/div/div/div/div/div[5]/input"
             ).click()
-        except Exception as e:
-            print(e.message)
+
+        except Exception:
+            pass
         time.sleep(5)
         i += 1
 
@@ -80,15 +73,14 @@ def image_search_scrape():
 
         try:
             # passing image urls one by one and downloading
-            urllib.request.urlretrieve(
-                i["src"], savePath + str(count) + ".jpg"
-            )
+            urllib.request.urlretrieve(i["src"], savePath + str(count) + ".jpg")
             count += 1
             print("Number of images downloaded = " + str(count), end="\r")
-        except Exception as e:
-            print(e.messsage)
+
+        except Exception:
+            pass
     print("Total images downloaded = " + count)
 
 
 if __name__ == "__main__":
-    image_search_scrape()
+    google_images_scraper()
