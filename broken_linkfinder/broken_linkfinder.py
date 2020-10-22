a7c99a4b2a377a637240fb81beab7f0bfbff0416
harshitashankar <harshitashankar@yahoo.com>
2020-10-22 14:49:14 +0530

    fix lint errors

--- broken_linkfinder/broken_linkfinder.py
+++ broken_linkfinder/broken_linkfinder.py
@@ -7,6 +7,7 @@ from urllib.parse import urljoin
 searched_links = []
 broken_links = []
 
+
 def getLinksFromHTML(html):
     def getLink(el):
         return el["href"]
@@ -14,7 +15,8 @@ def getLinksFromHTML(html):
 
 
 def find_broken_links(domainToSearch, URL, parentURL):
-    if (not (URL in searched_links)) and (not URL.startswith("mailto:")) and (not ("javascript:" in URL)) and (not URL.endswith(".png")) and (not URL.endswith(".jpg")) and (not URL.endswith(".jpeg")):
+    if (not (URL in searched_links)) and (not URL.startswith("mailto:")) and (not ("javascript:" in URL)) and \
+            (not URL.endswith(".png")) and (not URL.endswith(".jpg")) and (not URL.endswith(".jpeg")):
         try:
             requestObj = requests.get(URL)
             searched_links.append(URL)
