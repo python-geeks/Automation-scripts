a30898217c22dd93aaf4197405f21385c9278e74
Vilma Agalioti-Sgompou <vilma.v.m.a.s@gmail.com>
2020-10-29 22:03:36 +0000

    Create excel_inspector.py
    excel inspector in python

---
+++ excel_inspector/excel_inspector.py
@@ -0,0 +1,16 @@
+# Parse spreadsheets and its sheets ===========================================
+import pandas as pd
+import os
+
+sheets_folder = r''
+
+for path, subdirs, files in os.walk(sheets_folder):
+    for filename in files:
+        print('\n [] File:', filename, '===============')
+        if filename.endswith('.xlsx'):
+            excel = pd.ExcelFile(path + '\\' + filename)
+            print('Number of sheets:', len(excel.sheet_names))
+            print('Sheet names:', excel.sheet_names)
+            for sheet in excel.sheet_names:
+                df = excel.parse(sheet)
+                print('Sheet:', sheet, ' with the columns:', list(df.columns) )
