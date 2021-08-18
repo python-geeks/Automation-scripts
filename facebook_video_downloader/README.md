68f2d854c029c7ad3b582d8f34b7c1141c9d9e37
CodeBunny09 <pratikchoudhuri1609@gmail.com>
2021-08-18 22:53:14 +0530

    Added facebook video downloader

---
+++ facebook_video_downloader/README.md
@@ -0,0 +1,20 @@
+<h1>Using the Facebook Video Downloader</h1>
+
+<h3>Dependencies</h3>
+All you need to do is:
+
+```py
+pip install urllib3 requests
+```
+<h3>Downloading the video</h3>
+1. In the CMD or Shell run:
+```sh
+python main.py
+```
+2. Then you will be prompted for the url, and you have to enter the url of the video
+
+3. You will be prompted for the name in which the video will be saved. You can either enter the name of the video if you want, or leave it blank.
+
+4. Navigate to the `Downloads` directory and you'll find your video!
+
+<h4>It's that easy!!! 😁😁</h4>
---
+++ facebook_video_downloader/main.py
@@ -0,0 +1,29 @@
+import os
+import re
+import requests
+import urllib
+
+#Getting the link from user
+link = input('Enter the video url: ')
+name = input('Enter the name of the video: ')
+
+#Getting the HTML data
+
+html = requests.get(link)
+
+#Parsing the data
+try:
+    url = re.search('hd_src:"(.+?)"', html.text) #hd video
+    print('Found video in HD format.')
+except:
+    url = re.search('sd_src:"(.+?)"', html.text) #sd video
+    print('Found video in SD format.')
+
+# #download the file
+print('Downloading.......')
+
+if name:
+    urllib.request.urlretrieve(url[1], f"{os.path.join('Downloads', name)}.mp4")
+else:
+    urllib.request.urlretrieve(url[1], f"{os.path.join('Downloads', link[-6:])}.mp4")
+print('Video Downloaded Successfully')
