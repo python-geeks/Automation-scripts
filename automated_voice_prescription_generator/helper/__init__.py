e0df19bc4812029814db3123ae2deb8f2865f68a
sandipan898 <sandipan.das898@gmail.com>
2021-08-19 22:52:07 +0530

    Final changes

--- automated_voice_prescription_generator/helper/__init__.py
+++ automated_voice_prescription_generator/helper/__init__.py
@@ -1,2 +1,2 @@
-# from .pdf_op import save_pdf
+# from .pdf_operations import save_pdf
 # from .speech import speech_rec_for_windows, speech_rec_for_linux
--- automated_voice_prescription_generator/main.py
+++ automated_voice_prescription_generator/main.py
@@ -1,15 +1,13 @@
 import sys
-from helper.pdf_op import save_pdf
+from helper.pdf_operations import save_pdf
 from helper.speech import speech_rec_for_windows, speech_rec_for_linux
 
 
 def start_app():
     if sys.platform.startswith('linux'):
-        print("You are on Linux")
         medicines = speech_rec_for_linux()
 
     elif sys.platform.startswith('win32'):
-        print("You are on Windows")
         medicines = speech_rec_for_windows()
 
     print(medicines)
