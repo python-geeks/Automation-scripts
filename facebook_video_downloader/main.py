38f55d88aaf791563220e0be1bc0d585a85217c2
Pratik Choudhuri <pratikchoudhuri1609@gmail.com>
2021-08-19 20:20:44 +0530

    Update main.py

--- facebook_video_downloader/main.py
+++ facebook_video_downloader/main.py
@@ -3,25 +3,34 @@ import re
 import requests
 import urllib
 
-#Getting the link from user
+# Getting the link from user
+
+
 link = input('Enter the video url: ')
 name = input('Enter the name of the video: ')
 
-#Getting the HTML data
+
+# Getting the HTML data
+
 
 html = requests.get(link)
 
-#Parsing the data
+
+# Parsing the data
+
+
 try:
     url = re.search('hd_src:"(.+?)"', html.text) #hd video
     print('Found video in HD format.')
-except:
+except Exception as e:
     url = re.search('sd_src:"(.+?)"', html.text) #sd video
     print('Found video in SD format.')
 
-# #download the file
-print('Downloading.......')
+    
+# download the file
 
+
+print('Downloading.......')
 if name:
     urllib.request.urlretrieve(url[1], f"{os.path.join('downloaded_videos', name)}.mp4")
 else:
