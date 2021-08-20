99209023e2d806de98affe443abfe63031e3fe7e
Shreyas Vedpathak <shreyasvedpathak@gmail.com>
2021-08-20 16:56:06 +0530

    Added PR number field, and underline utility

--- certificate_generator/app/routes.py
+++ certificate_generator/app/routes.py
@@ -3,19 +3,32 @@ from app.utils import generate_certificate
 from flask import render_template, request, send_file
 import os
 
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
    return render_template("certificate.html")
 
 
-@app.route('/download', methods=['GET', 'POST'])
-def download_certificate():
+@app.route('/render', methods=['POST'])
+def render_certificate():
     if request.method == "POST":
-        file_name = generate_certificate(request.form['name'])
+        file_name = generate_certificate(request.form['name'], request.form['pr_num'])
         return render_template('download.html', file_name=file_name)
 
 @app.route('/download_certificate', methods=['GET'])
-def method_name():
+def download():
     if request.method == "GET":
         filename = request.args.get("filename")
         filepath = os.path.join("static/certificates/generated", filename)
--- certificate_generator/app/utils.py
+++ certificate_generator/app/utils.py
@@ -2,7 +2,7 @@ from PIL import Image, ImageFont, ImageDraw
 from app import app
 import os
 
-def generate_certificate(name):
+def generate_certificate(name, pr_num):
     template_dir = os.path.join(app.root_path, "static/certificates/template")
     template_filename = "template.png"
     
@@ -12,8 +12,25 @@ def generate_certificate(name):
     
     img = Image.open(os.path.join(template_dir, template_filename))
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype(os.path.join(template_dir, "DroidSansMono.ttf"), 150)
-    draw.text((1000, 1390), name, (0, 0, 0), font=font)
+    
+    event_name = "Hacktoberfest"
+    contributed_at = "Automation scripts"
+    msg = f"""for taking part in {event_name} and
+contribution in #{pr_num} at {contributed_at}.
+"""
+    
+    name_font = ImageFont.truetype(os.path.join(template_dir, "Sanchez-Regular.ttf"), 150)
+    msg_font = ImageFont.truetype(os.path.join(template_dir, "Sanchez-Regular.ttf"), 70)
+    
+    draw.text((1000, 1390), name, (51, 213, 172), font=name_font)
+    draw.text((1000, 1650), msg, (14, 69, 115), font=msg_font)
+    
+    twidth, theight = draw.textsize(contributed_at, font=msg_font)
+    prwidth, prheight = draw.textsize(pr_num, font=msg_font)
+    
+    lx = 1560 + prwidth + 120
+    ly = 1810
+    draw.line((lx, ly, lx + twidth, ly), fill=(14, 69, 115), width=5)
     
     img.save(os.path.join(output_dir, output_filename))
     
