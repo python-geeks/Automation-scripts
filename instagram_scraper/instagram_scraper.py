35b7f6fab4847d720d491187e234ea81b6638dfd
Aashutosh <aashnirwal@gmail.com>
2021-08-26 13:54:25 +0530

    instagram scraper comleted version 1

--- instagram_scraper/instagram_scraper.py
+++ instagram_scraper/instagram_scraper.py
@@ -1,21 +1,45 @@
-# import selenium, time
+# import selenium, time, urllib.request
 from selenium import webdriver
-import time
+import time,urllib.request
 
-
+profileUrl = input("enter profile url to scrape: ")
 # open chrome and go to the set page
 driver = webdriver.Chrome()
-driver.get("https://www.instagram.com/funnywhimsical/")
-
+# set url of instagram page
+driver.get(profileUrl)
 
 # scroll to the bottom of the page
 pageLength = driver.execute_script("window.scrollTo(0, document.body.scrollHeight);"
                                    "var pageLength=document.body.scrollHeight;return pageLength;")
-match = False
-while(match == False):
+match = True
+while match:
     lastCount = pageLength
-    time.sleep(2)
     pageLength = driver.execute_script("window.scrollTo(0, document.body.scrollHeight);"
                                        "var pageLength=document.body.scrollHeight;return pageLength;")
     if lastCount == pageLength:
-        match = True
+        match = False
+
+# find all links that match '/p' and append to list named userPosts
+userPosts = []
+links = driver.find_elements_by_tag_name('a')
+for link in links:
+    post = link.get_attribute('href')
+    if '/p/' in post:
+        userPosts.append(post)
+
+
+download_url = ''
+# extract shortcode and identify post type, then download them as jpg or mp4 depending on post type
+for post in userPosts:
+    driver.get(post)
+    shortcode = driver.current_url.split("/")[-2]
+    postType = driver.find_element_by_xpath('//meta[@property="og:type"]').get_attribute('content')
+    if postType == 'video':
+        download_url = driver.find_element_by_xpath("//meta[@property='og:video']").get_attribute('content')
+        urllib.request.urlretrieve(download_url, '{}.mp4'.format(shortcode))
+    else:
+        download_url = driver.find_element_by_xpath("//meta[@property='og:image']").get_attribute('content')
+        urllib.request.urlretrieve(download_url, '{}.jpg'.format(shortcode))
+    time.sleep(5)
+# close connections and chrome once all posts are downloaded
+driver.close()
