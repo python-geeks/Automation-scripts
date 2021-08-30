8f7f9c06d6ffebe97db07f92d5ebfadf22592da8
abhishekchopra0907 <51484719+abhishekchopra0907@users.noreply.github.com>
2021-08-30 23:57:05 +0530

    cleaning

--- gender_detector/detect_gender_webcam.py
+++ gender_detector/detect_gender_webcam.py
@@ -2,30 +2,27 @@ from keras.preprocessing.image import img_to_array
 from keras.models import load_model
 from keras.utils import get_file
 import numpy as np
-import argparse
 import cv2
-import os
 import cvlib as cv
-                     
-# load model
+
+#load model
 dwnld_link = "https://github.com/arunponnusamy/cvlib/releases/download/v0.2.0/gender_detection.model"
 model_path = get_file("gender_detection.model", dwnld_link,
                      cache_subdir="pre-trained", cache_dir=os.getcwd())
 
 model = load_model(model_path)
-# open webcam
+#open_webcam
 webcam = cv2.VideoCapture(0)
 
 if not webcam.isOpened():
     print("Could not open webcam")
     exit()
     
-classes = ['man','woman']
+classes = ['man' , 'woman']
 
 # loop through frames
 while webcam.isOpened():
-
-    # read frame from webcam 
+    # read frame from webcam
     status, frame = webcam.read()
 
     if not status:
@@ -40,8 +37,7 @@ while webcam.isOpened():
 
     # loop through detected faces
     for idx, f in enumerate(face):
-
-        # get corner points of face rectangle        
+        # get corner points of face rectangle    
         (startX, startY) = f[0], f[1]
         (endX, endY) = f[2], f[3]
 
@@ -64,26 +60,26 @@ while webcam.isOpened():
         conf = model.predict(face_crop)[0]
         print(conf)
         print(classes)
-
+        
         # get label with max accuracy
         idx = np.argmax(conf)
         label = classes[idx]
-
+        
         label = "{}: {:.2f}%".format(label, conf[idx] * 100)
-
+        
         Y = startY - 10 if startY - 10 > 10 else startY + 10
-
+        
         # write label and confidence above face rectangle
         cv2.putText(frame, label, (startX, Y),  cv2.FONT_HERSHEY_SIMPLEX,
                     0.7, (0, 255, 0), 2)
-
+        
     # display output
     cv2.imshow("gender detection", frame)
-
+    
     # press "Q" to stop
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
+        
 # release resources
 webcam.release()
 cv2.destroyAllWindows()
