99b4ecb1ca931f3b57a99c815336d7d55c3ea4b4
abhishekchopra0907 <51484719+abhishekchopra0907@users.noreply.github.com>
2021-08-31 11:05:15 +0530

    Update detect_gender_webcam.py

--- gender_detector/detect_gender_webcam.py
+++ gender_detector/detect_gender_webcam.py
@@ -8,7 +8,7 @@ import cvlib as cv
 
 # load model
 dwnld_link = "https://github.com/arunponnusamy/cvlib/releases/download/v0.2.0/gender_detection.model"
-model_path = get_file("gender_detection.model", dwnld_link,cache_subdir="pre-trained", cache_dir=os.getcwd())
+model_path = get_file("gender_detection.model", dwnld_link, cache_subdir="pre-trained", cache_dir=os.getcwd())
 
 model = load_model(model_path)
 # open_webcam
@@ -66,8 +66,7 @@ while webcam.isOpened():
         label = "{}: {:.2f}%".format(label, conf[idx] * 100)
         Y = startY - 10 if startY - 10 > 10 else startY + 10
         # write label and confidence above face rectangle
-        cv2.putText(frame, label, (startX, Y),  cv2.FONT_HERSHEY_SIMPLEX,
-                    0.7, (0, 255, 0), 2)
+        cv2.putText(frame, label, (startX, Y),  cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
     # display output
     cv2.imshow("gender detection", frame)
     # press "Q" to stop
