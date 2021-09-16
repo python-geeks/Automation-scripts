84ec2302b2d650e1533fb992e49f9a275281fb01
LetsGetStartedWithBub <letsgetstartedwithbub@gmail.com>
2021-09-17 03:37:05 +0530

    Added Readme and requirements.txt

--- slideshare_downloader/downloader.py
+++ slideshare_downloader/downloader.py
@@ -2,10 +2,10 @@
 This file can download slide share presentations on your local system.
 without logging in :)
 Steps to proceed ->
-1). The url of the ppt is being provided
-2). Using beautiful soup the tag section inside which the images of ppt
-    are available  is scraped.
-3). Scraped images are combined together using pyttx (forming a presentation)
+1). The url of the ppt should be specified in url variables.
+2). Using beautiful soup presentation images links are scrapped.
+3). Scraped images are downloaded and combined together using pyttx.
+4). Presentation is formed.
 """
 import io
 import os
@@ -16,20 +16,17 @@ from pptx import Presentation
 url = 'https://www.slideshare.net/Slideshare/get-started-with-slide-share'
 
 data = requests.get(url)
-my_data = []
 
 html = BeautifulSoup(data.text, 'html.parser')
 links = html.select('img.slide_image')
 prs = Presentation()
 for link in links:
     image = link["data-full"]
-    my_data.append(image)
     response = requests.get(image)
     f = io.BytesIO(response.content)
     slide = prs.slides.add_slide(prs.slide_layouts[8])
     placeholder = slide.placeholders[1]
     picture = placeholder.insert_picture(f)
 
-prs.save("ESEMPIO.pptx")
-os.startfile("ESEMPIO.pptx")
-print(len(my_data))
+prs.save("Presentation.pptx")
+os.startfile("Presentation.pptx")
