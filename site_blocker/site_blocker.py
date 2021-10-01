3f11d5d8451cd0aaad134816cbbfa06dddb33f84
ujjwaltwitx <ujjwalcpj123@gmail.com>
2021-10-01 22:44:27 +0530

    solved the flake8 error for whitespace

--- site_blocker/site_blocker.py
+++ site_blocker/site_blocker.py
@@ -2,7 +2,7 @@
 def add_domain():
     domain_name = input("Enter the domain of the website to block:")
     with open("c:/Windows/System32/drivers/etc/hosts", "a") as fileHandler:
-        fileHandler.write('127.0.0.1 {}'.format(domain_name)+'\n')
+        fileHandler.write('127.0.0.1 {}'.format(domain_name) + '\n')
 
 
 # function for removing the domains from the host file
