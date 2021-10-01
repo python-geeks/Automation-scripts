a2ea533afb90ca1c8e6177c40576dd31dcb78aad
ujjwaltwitx <ujjwalcpj123@gmail.com>
2021-10-01 21:23:08 +0530

    feat: Added site_blocker

--- site_blocker/site_blocker.py
+++ site_blocker/site_blocker.py
@@ -10,7 +10,7 @@ def remove_all_domains():
 	all_domains_list = input("Enter the domains to remove separated by space: ")
 	all_domains = all_domains_list.split(" ")
 	start_pos = 0
-	with open("c:/Windows/System32/drivers/etc/hosts", 'r+') as  f:
+	with open("c:/Windows/System32/drivers/etc/hosts", 'r+') as f:
 		file_contents = f.readlines()
 		for each_domain in all_domains:
 			for each_line in file_contents:
@@ -22,13 +22,14 @@ def remove_all_domains():
 		f.seek(0)	
 		f.truncate()
 		f.writelines(file_contents)
-		print("All requested sites have been blocked")
-
-# Just for testing and fun I wrote this lines of code but it was amazing the number of automation scripts that have been done so far by me
-choice = int(input("Enter 1 to block site and 2 to remove a blocked site:"))
-if choice == 1:
-	add_domain()
-elif choice == 2:
-	remove_all_domains()
-else:
-	print("Wrong Choice")
+		print("All requested sites have been unblocked")
+try:
+	choice = int(input("Enter 1 to block site and 2 to remove a blocked site:"))
+	if choice == 1:
+		add_domain()
+	elif choice == 2:
+		remove_all_domains()
+	else:
+		print("Wrong Choice")
+except:
+	print("Not a valid choice")
