b7163accdceef92901309d6dce6b2d387acbd78f
Sonu <sonusaha110@gmail.com>
2021-10-02 11:52:09 +0530

    Add keys here

---
+++ retweet_bot/keys.py
@@ -0,0 +1,6 @@
+# Twitter application consumer keys and access tokens.
+# Remember, keep this information private.
+consumer_key = 'XXXXXXXXXX'
+consumer_secret = 'XXXXXXXXXX'
+access_token = 'XXXXXXXXXX'
+access_token_secret = 'XXXXXXXXXX'
