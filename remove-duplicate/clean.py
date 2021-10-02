a02eb74af0fbab970c6b8d2bd59f6b63328bed61
jancoxx412 <40721310+jancoxx412@users.noreply.github.com>
2021-10-03 06:29:44 +0700

    Update clean.py

--- remove-duplicate/clean.py
+++ remove-duplicate/clean.py
@@ -10,11 +10,11 @@ print("[+] Start Remove Duplicate ...\n\n")
 time.sleep(1)
 count = 0
 result = listnya.replace(".txt", "")
-file = result+'-clean.txt'
+file = result + '-clean.txt'
 folder = str(output)
 for i in tqdm.tqdm(cok):
     time.sleep(0)
     count += 1
-    open(folder+file, 'a').write(i)
-final = total-count
+    open(folder + file, 'a').write(i)
+final = total - count
 print("\nSuccess Remove {} From list".format(str(final)))
