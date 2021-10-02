aabcaa84c9893f1af8838b55a612e492c434d297
Sonu <sonusaha110@gmail.com>
2021-10-02 12:08:00 +0530

    Linting checked

--- retweet_bot/keys.py
+++ retweet_bot/keys.py
@@ -1,6 +1,6 @@
 # Twitter application consumer keys and access tokens.
 # Remember, keep this information private.
-consumer_key = 'XXXXXXXXXX'
-consumer_secret = 'XXXXXXXXXX'
-access_token = 'XXXXXXXXXX'
-access_token_secret = 'XXXXXXXXXX'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
