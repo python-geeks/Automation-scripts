9fe2c38831d64e488409aaed1b0a25aa3485b069
pawangeek <pawanjain.43210@gmail.com>
2021-10-03 10:18:03 +0530

    attendence automator [linting]

--- attendance-automator/main.py
+++ attendance-automator/main.py
@@ -71,10 +71,10 @@ while True:
 
             y1, x2, y2, x1 = faceLoc
 
-            y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.rectangle(frame, (x1, y2-35), (x2, y2), (0, 255, 0), cv2.FILLED)
-            cv2.putText(frame, name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 1)
+            cv2.rectangle(frame, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.putText(frame, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 1)
             attendance(name)
     cv2.imshow("Camera", frame)
 
