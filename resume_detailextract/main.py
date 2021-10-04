3d528cd125329809429e0252c9b971e41c6270ee
some-earth11 <some_earth@outlook.com>
2021-10-05 04:54:34 +0530

    Added Resume Detail Extractor

---
+++ resume_detailextract/main.py
@@ -0,0 +1,10 @@
+# All Resume Parser Dependencies must be installed
+# Java must be running
+
+
+from resume_parser import resumeparse
+
+data = resumeparse.read_file(r"C:\Users\some_\Downloads\Profile.pdf")
+
+
+# PS: First Run takes a while
