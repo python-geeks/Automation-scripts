1db71b0c36aab1f4d8dc991b124e8ae55cebcda2
pccoder-2000 <chaturvedipriya998@gmail.com>
2021-10-05 00:55:17 +0530

    Fixed lint

--- chat_bot/rule_based_chatbot.py
+++ chat_bot/rule_based_chatbot.py
@@ -1,73 +1,74 @@
 import nltk
-from nltk.chat.util import Chat,reflections
+from nltk.chat.util import Chat, reflections
 
 print(Chat)
 
 reflections
-set_pairs=[
+
+Set_pairs = [
     [
         r"my name is (.*)",
-        ["Hello %l, How are you today?",]
+        ["Hello %l, How are you today?", ]
     ],
     [
         r"hi|hey|hello",
-        ["Hello", "Hey there",]
+        ["Hello", "Hey there", ]
     ],
     [
         r"(.*)help(.*) ",
-        ["I can help you ",]
+        ["I can help you ", ]
     ],
     [
         r"(.*) your name ?",
-        ["My name is beedo, but you can just call me robot and I'm a chatbot .",]
+        ["My name is beedo, but you can just call me robot & I'm a chatbot.", ]
     ],
     [
         r"how are you (.*) ?",
-        ["I'm doing very well", "i am great !"]
+        ["I'm doing very well", "i am great !", ]
     ],
     [
         r"sorry (.*)",
-        ["Its alright","Its OK, never mind that",]
+        ["Its alright", "Its OK, never mind that", ]
     ],
     [
         r"i'm (.*) (good|well|okay|ok)",
-        ["Nice to hear that","Alright, great !",]
+        ["Nice to hear that", "Alright, great !", ]
     ],
     [
         r"(hi|hey|hello|hola|holla)(.*)",
-        ["Hello", "Hey there",]
+        ["Hello", "Hey there", ]
     ],
     [
         r"what (.*) want ?",
-        ["Make me an offer I can't refuse",]
+        ["Make me an offer I can't refuse", ]
     ],
     [
         r"(.*)created(.*)",
-        ["XYZ created me using Python's NLTK library ","top secret ;)",]
+        ["XYZ created me using Python's NLTK library ", "top secret ;)", ]
     ],
     [
         r"(.*) (location|city) ?",
-        ['New Delhi, India',]
+        ['New Delhi, India', ]
     ],
     [
         r"(.*)raining in (.*)",
-        ["No rain in the past 4 days here in %2","In %2 there is a 50% chance of rain",]
+        ["No rain in the past 4 days here in %2", ]
     ],
     [
         r"how (.*) health (.*)",
-        ["Health is very important, but I am a computer, so I don't need to worry about my health ",]
+        ["I am a computer, so I don't need to worry about my health ", ]
     ],
     [
         r"(.*)(sports|game|sport)(.*)",
-        ["I'm a very big fan of Cricket",]
+        ["I'm a very big fan of Cricket", ]
     ],
     [
         r"who (.*) (Cricketer|Batsman)?",
-        ["Virat Kohli"]
+        ["Virat Kohli", ]
     ],
     [
         r"quit",
-        ["Bye for now. See you soon :) ","It was nice talking to you. See you soon :)"]
+        ["Bye for now. See you soon :) ", "It was nice talking to you.", ]
     ],
     [
         r"(.*)",
@@ -75,15 +76,17 @@ set_pairs=[
     ],
 ]
 
+
 def chatbot():
     print("Hi I am a rule-based chatbot ! what can i help you with?")
+
+
 chatbot()
 
 
-chat = Chat(set_pairs,reflections)
+chat = Chat(Set_pairs, reflections)
 print(chat)
 
 chat.converse()
 if __name__ == "__main__":
     chatbot()
-
