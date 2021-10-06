f211a0b13943ae389072d82800faebbeda74ed0b
Pawan Jain <42181691+pawangeek@users.noreply.github.com>
2021-10-06 10:40:40 +0530

    Update yaml_to_json.py

--- yaml_to_json/yaml_to_json.py
+++ yaml_to_json/yaml_to_json.py
@@ -1,25 +1,25 @@
 from ruyaml import YAML
 import json
-print("Welcome to Yaml to Json converter!")
+
 
 def get_yaml_data():
     yaml_name = input("Enter the yaml file name: ")
+
     try:
-        with open(yaml_name,"r+") as f:
+        with open(yaml_name, "r+") as f:
             yaml_data = YAML().load(f)
             return yaml_data
-
     except:
         print("Invalid input enter a valid yaml file name e.g. example.yaml")
         yaml_data = get_yaml_data()
 
-    
+
 def convert_to_json(yaml_data):
     json_name = input("Enter the name of output json file: ")
-    try:       
-        with open(json_name,"w+") as o:
-            o.write(json.dumps(yaml_data))
 
+    try:
+        with open(json_name, "w+") as o:
+            o.write(json.dumps(yaml_data))
     except:
         print("Invalid input enter a valid json file name e.g. example.json")
         convert_to_json(yaml_data)
@@ -27,4 +27,4 @@ def convert_to_json(yaml_data):
 yaml_data = get_yaml_data()
 convert_to_json(yaml_data)
 
-print(f"Your yaml file has been converted and saved as json")
+print("Your yaml file has been converted and saved as json")
