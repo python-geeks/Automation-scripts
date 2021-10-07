c271958cdb28e2b7d2df1891f0b19b79b0910e39
Richa812 <richariya172000@gmail.com>
2021-10-07 18:29:11 +0530

    Readme Added

---
+++ Calendar_GUI/Code.py
@@ -0,0 +1,52 @@
+
+from tkinter import *
+import calendar
+
+
+def showCal() :
+
+	new_gui = Tk()
+	new_gui.config(background = "white")
+	new_gui.title("CALENDAR")
+	new_gui.geometry("550x600")
+	fetch_year = int(year_field.get())
+	cal_content = calendar.calendar(fetch_year)
+	cal_year = Label(new_gui, text = cal_content, font = "Consolas 10 bold")
+	cal_year.grid(row = 5, column = 1, padx = 20)
+	new_gui.mainloop()
+
+	
+
+if __name__ == "__main__" :
+
+
+	gui = Tk()
+	gui.config(background = "white")
+	gui.title("CALENDAR")
+	gui.geometry("250x140")
+	cal = Label(gui, text = "CALENDAR", bg = "white",
+							font = ("times", 28, 'bold'))
+
+	year = Label(gui, text = "Enter Year", bg = "light blue")
+	year_field = Entry(gui)
+
+	Show = Button(gui, text = "Show Calendar", fg = "Black",
+							bg = "Red", command = showCal)
+
+	
+	Exit = Button(gui, text = "Exit", fg = "Black", bg = "Red", command = exit)
+	
+
+	cal.grid(row = 1, column = 1)
+
+	year.grid(row = 2, column = 1)
+
+	year_field.grid(row = 3, column = 1)
+
+	Show.grid(row = 4, column = 1)
+
+	Exit.grid(row = 6, column = 1)
+	
+	# start the GUI
+	gui.mainloop()
+	
---
+++ Calendar_GUI/Readme.md
@@ -0,0 +1,31 @@
+# Calendar GUI
+
+## Modules used:
+- Tkinter
+- calendar
+
+## Installing the Modules:
+
+- FOR Linux Based System:
+
+>_sudo apt-get install python3-tk_
+
+- FOR Windows Based System:
+
+>_The simplest method to install Tkinter in a Windows environment is to download and install either ActivePython 3.8 or 3.7_
+
+
+>_Alternatively, you can create and activate a Conda environment with Python 3.7 or greater that is integrated with the latest version of Tkinter_
+
+
+## How To Use
+
+- Clone this repository to your local machine
+- Run the python code
+- A Window asking for the year to be displayed will be opened
+- Enter the year whose calendar you wnat to see
+- You will see a window with the calendar of the particular year.
+
+
+
+
