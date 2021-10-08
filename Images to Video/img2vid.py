9d93628d34121bc13404f70769d15cf19c93edc2
Pawan Jain <42181691+pawangeek@users.noreply.github.com>
2021-10-08 11:25:45 +0530

    Update img2vid.py

--- Images to Video/img2vid.py
+++ Images to Video/img2vid.py
@@ -1,7 +1,6 @@
 import cv2
 
 from imutils import paths
-import os
 import argparse
 
 ap = argparse.ArgumentParser()
@@ -10,22 +9,23 @@ ap.add_argument("-o", "--output_video", type=str, required=True, help="output vi
 args = vars(ap.parse_args())
 
 out = None
-(w,h) = (None,None)
+(w, h) = (None, None)
 img_array = []
 
 imagePaths = list(paths.list_images(args["path"]))
 
 for filename in imagePaths:
     img = cv2.imread(filename)
-    img = cv2.resize(img,(1400,800))
+    img = cv2.resize(img, (1400, 800))
     height, width, layers = img.shape
     if w is None or h is None:
-        (w,h) = (width,height)
+        (w, h) = (width, height)
     img_array.append(img)
-	
+
 if out is None:
-    out = cv2.VideoWriter(args["output_video"],cv2.VideoWriter_fourcc(*'DIVX'), 1, (w,h),True)
-	
+    out = cv2.VideoWriter(args["output_video"], cv2.VideoWriter_fourcc(*'DIVX'), 1, (w, h), True)
+
 for i in range(len(img_array)):
     out.write(img_array[i])
+
 out.release()
