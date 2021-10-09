5a88e5e5f14fe2f1fd8c0ca78c58c8c730023075
B.Hariprasad <hariprasad19036@cse.ssn.edu.in>
2021-10-09 22:10:51 +0530

    python3 script for downloading subtitles from youtube videos

---
+++ subtitle_downloader/subtitle_downloader.py
@@ -0,0 +1,17 @@
+# Importing necessary modules
+import sys
+from youtube_transcript_api import YouTubeTranscriptApi
+
+if len(sys.argv) != 2:
+    print("Enter valid number of arguments\nTry python subtitle_downloader.py <video link>")
+    sys.exit()
+
+# The list of dictionaries obtained by the the get_transcript() function
+# is stored in sub variable
+url = sys.argv[1]
+sub = YouTubeTranscriptApi.get_transcript(url.split("?v=")[1])
+ 
+# Writing subtitles into a file.
+with open("subtitles.txt", "w") as f:
+    for i in sub:
+        f.write("{}\n".format(i))
