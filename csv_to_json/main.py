9bc6f0695882cb9e37a2cd59b86f270dc36dbf7c
durgeshahire07 <durgeshahire07@gmail.com>
2021-10-09 05:49:44 +0530

    resolved flake8linter error

--- csv_to_json/main.py
+++ csv_to_json/main.py
@@ -9,7 +9,7 @@ assert args, "No file was provided; Provide a CSV filename"
 isdir = os.path.isdir(args[-1])
 # If input is CSV file
 if not isdir:
-    assert args[-1].endswith(".csv"), "Input file is not CSV; Provide CSV filename with .csv extension"
+    assert args[-1].endswith(".csv"), "Input file is not CSV"
     data = pd.read_csv(args[-1])  # load CSV data as Pd object
-    data.to_json(args[-1][:-4] + ".json", orient='records') 
+    data.to_json(args[-1][:-4] + ".json", orient='records')
     del data
