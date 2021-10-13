0161fb61c8a92e4889f6540ab2ff8613fdb645d5
Joaquin Montesinos <jkm280294@gmail.com>
2021-10-13 16:55:02 +0200

    flake8

--- email_feed/gmail_feed.py
+++ email_feed/gmail_feed.py
@@ -9,8 +9,7 @@ URL = 'https://%s:%s@mail.google.com/mail/u/0/feed/atom/all' % (username, passwo
 r = requests.get(URL)
 
 if r.status_code == 401:
-    print("login [%s] or password [%s] is incorrect\n%s" % (username, password, 
-                                                            'Also try enable "Allow less secure apps" on https://myaccount.google.com/lesssecureapps and |Gmail Settings -> Forwarding and POP / IMAP -> IMAP Acess to Enable IMAP| '))
+    print("login [%s] or password [%s] is incorrect\n%s" % (username, password,  'Error'))
 elif r.status_code != 200:
     print("Requests error [%s] - %s" % (r.status_code, URL))
 elif r.status_code == 200:
@@ -21,7 +20,7 @@ elif r.status_code == 200:
         key_words1 = re.findall('unsusbscribe', text)
         key_words2 = re.findall('Stop these mails', text)
         keys = key_words1 + key_words2
-        urls = re.findall('''(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-&?=%.]+''', text)
+        urls = re.findall(r'''(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-&?=%.]+''', text)
         if len(keys) > 0 and len(urls)> 0:
             print(a['feed']['entry'][k]['title'])
             print(a['feed']['entry'][k]['summary'][0:50])
