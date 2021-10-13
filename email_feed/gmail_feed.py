d213a37edef89a9564dfc2ed493f5f196d54959f
Joaquin Montesinos <jkm280294@gmail.com>
2021-10-13 16:51:34 +0200

    flake8

--- email_feed/gmail_feed.py
+++ email_feed/gmail_feed.py
@@ -1,29 +1,27 @@
 import xmltodict
 import re
 import requests
-import datetime
-from datetime import datetime, timedelta
 
-username="xxx"
-password="xxx"
+username = "xxx"
+password = "xxx"
 
 URL = 'https://%s:%s@mail.google.com/mail/u/0/feed/atom/all' % (username, password)
 r = requests.get(URL)
 
 if r.status_code == 401:
-    print("login [%s] or password [%s] is incorrect\n%s" % (username, password, 'Also try enable "Allow less secure apps" on https://myaccount.google.com/lesssecureapps and |Gmail Settings -> Forwarding and POP / IMAP -> IMAP Acess to Enable IMAP| '))
+    print("login [%s] or password [%s] is incorrect\n%s" % (username, password, 
+                                                            'Also try enable "Allow less secure apps" on https://myaccount.google.com/lesssecureapps and |Gmail Settings -> Forwarding and POP / IMAP -> IMAP Acess to Enable IMAP| '))
 elif r.status_code != 200:
     print("Requests error [%s] - %s" % (r.status_code, URL))
 elif r.status_code == 200:
     contents = r.text
     a = xmltodict.parse(contents)
-    now_date=datetime.now()
     for k in range(len(a['feed']['entry'])):
         text = a['feed']['entry'][k]['summary']
         key_words1 = re.findall('unsusbscribe', text)
         key_words2 = re.findall('Stop these mails', text)
-        keys=key_words1+key_words2
-        urls = re.findall('(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-&?=%.]+', text)
+        keys = key_words1 + key_words2
+        urls = re.findall('''(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-&?=%.]+''', text)
         if len(keys) > 0 and len(urls)> 0:
             print(a['feed']['entry'][k]['title'])
             print(a['feed']['entry'][k]['summary'][0:50])
