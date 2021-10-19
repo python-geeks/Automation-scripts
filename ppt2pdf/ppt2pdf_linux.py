132a8f3774c6f5f5c77abcb2d687472c2116e554
Pawan Jain <42181691+pawangeek@users.noreply.github.com>
2021-10-19 20:44:03 +0530

    Update ppt2pdf_linux.py

--- ppt2pdf/ppt2pdf_linux.py
+++ ppt2pdf/ppt2pdf_linux.py
@@ -6,7 +6,7 @@ import os
 PATH = "INPUT FOLDER"
 # extension
 et = "pptx"  # or ppt
-files = [f for f in glob.glob(PATH + "/**/*.{}".format(et),recursive =True)]
+files = [f for f in glob.glob(PATH + "/**/*.{}".format(et), recursive=True)]
 for f in tqdm.tqdm(files):
     command = "unoconv -f pdf \"{}\"".format(f)
     os.system(command)
