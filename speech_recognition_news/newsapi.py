c2a9d7639af37cd514f7630de7fa9d6c98577bdf
KazushiR <38971729+KazushiR@users.noreply.github.com>
2021-10-19 14:28:58 -0700

    adding speech recognition news

---
+++ speech_recognition_news/newsapi.py
@@ -0,0 +1,56 @@
+import os
+import requests
+import json
+from dotenv import load_dotenv, find_dotenv
+from newspaper import Article
+from newspaper import Config
+from datetime import datetime
+from datetime import datetime
+
+news_information = dict()
+
+failed_port_connection = []
+
+now = datetime.now()
+todays_date = now.strftime("%b %d, %Y")
+
+
+def get_top_news(news_information, todays_date):
+    load_dotenv(find_dotenv(r"path to environmental variable"))
+    news_api_key = os.getenv("news_api")
+    params = (
+        ('country', 'us'),
+        ('apiKey', f'{news_api_key}'),
+    )
+    response = requests.get(
+        """https://newsapi.org/v2/top-headlines""",
+        params=params)
+    news_articles = response.json()['articles']
+    for i in news_articles:
+        user_agent = """Mozilla/5.0 (Macintosh;
+        Intel Mac OS X 10.15; rv:78.0)
+        Gecko/20100101 Firefox/78.0"""
+        config = Config()
+        config.browser_user_agent = user_agent
+        config.request_timeout = 10
+        article = Article(i['url'], config=config)
+        print(i["url"])
+        article.download()
+        article.parse()
+        article.nlp()
+        summary = article.summary
+        news_information[i["source"]["name"]] = {
+            """title": i["title"],
+            "summary":summary, "url":i["url"],
+                "date":f"{todays_date}"""
+                }
+
+
+def json_file(news_information, todays_date):
+    json_file = json.dumps(news_information, indent=4)
+    with open(f"news_summary/news_summary {todays_date}.txt", "w") as outfile:
+        outfile.write(json_file)
+        outfile.close()
+
+
+
---
+++ speech_recognition_news/readme.txt
@@ -0,0 +1,8 @@
+-to execute the script, the dotenv and newspaper3 package will need to be downloaded. Documentation can be found below.
+	newspaper3: https://newspaper.readthedocs.io/en/latest/
+	dotenv: https://pypi.org/project/python-dotenv/
+From there, you will need to go to https://newsapi.org/ and create an account and get the api key". Documentation for this can be found per below.
+	Getting Started: https://newsapi.org/docs/get-started#search
+	Python: https://newsapi.org/docs/client-libraries/python
+
+Once you have the required packages, execute the scipt and it should pull the latest news articles, summarize them and put it into a json format under the folder "news_summary".
