3dae10e572fe0917c69db41a7a5aaa61c25dd876
shanmukhavishnu <50124557+iam-shanmukha@users.noreply.github.com>
2021-10-20 12:22:55 +0530

    followed flake8 linitng, Fix #767

--- covid19_telegram_bot/covid19_telegram_bot.py
+++ covid19_telegram_bot/covid19_telegram_bot.py
@@ -14,8 +14,8 @@ def send_msg(text):
     chat_id = "GROUP-CHAT-ID-HERE"
     url_req = (
         'https://api.telegram.org/'
-        'bot' + token + '/sendMessage' +
-        '?chat_id=' + chat_id + "&text=" + text)
+        'bot' + token + '/sendMessage' + '?chat_id'
+        '=' + chat_id + "&text=" + text)
     results = requests.get(url_req)
     print(results.json())
 
