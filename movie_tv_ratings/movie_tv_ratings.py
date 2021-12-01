cd7e7e540ef45e6426eac6076c7078e9cd050e63
Waad AlKhenji <alkhenji.waad@gmail.com>
2021-12-02 01:18:30 +0300

    Movie and TV ratings scraper - fixing lint with flake8 errors

--- movie_tv_ratings/movie_tv_ratings.py
+++ movie_tv_ratings/movie_tv_ratings.py
@@ -2,6 +2,7 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
+
 # Gets top 250 movies from IMDB
 def scrape_movies():
 
@@ -14,8 +15,8 @@ def scrape_movies():
     for i in range(len(movies)):
         movie_string = movies[i].get_text()
         movie = (' '.join(movie_string.split()).replace('.', ''))
-        movie_title = movie[len(str(i))+1:-7]
-        print(("| " + str(i+1)) + (" | " + movie_title) + (" | Rating : " + "{:.1f}".format(float(ratings[i]))))
+        movie_title = movie[len(str(i)) + 1:-7]
+        print(("| " + str(i + 1)) + (" | " + movie_title) + (" | Rating : " + "{:.1f}".format(float(ratings[i]))))
     return
 
 
@@ -24,7 +25,7 @@ def scrape_tvshows():
     page = requests.get("https://www.imdb.com/chart/toptv")
     Results = re.findall(r'" alt="(.+?)".*?title="(.*?)".*?strong.*?"(.*?)"', page.text, re.DOTALL)
     for i in range(len(Results)):
-        print("| " + str(i+1) + " | " + Results[i][0] + " | Rating : " + Results[i][-1][:3])
+        print("| " + str(i + 1) + " | " + Results[i][0] + " | Rating : " + Results[i][-1][:3])
 
     return
 
@@ -40,12 +41,13 @@ while (val):
     if val == 'Movies':
         globals()['scrape_movies']()
         print("\n")
-        val = input("Type 'Movies' or 'TV' or 'exit': ")  
+        val = input("Type 'Movies' or 'TV' or 'exit': ")
     elif val == 'TV':
         globals()['scrape_tvshows']()
         print("\n")
-        val = input("Type 'Movies' or 'TV' or 'exit': ") 
+        val = input("Type 'Movies' or 'TV' or 'exit': ")
     elif val == 'exit':
         val = ''
     else:
         val = input("Wrong Input. Try Again: ")
+        
