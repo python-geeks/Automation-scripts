44f457aedaab86651ff6f7aaac83241fd023bfc1
Palani <palanioffcl@gmail.com>
2022-10-02 07:38:55 +0530

    Update dirbrute.py

--- directory_bruteforcing/dirbrute.py
+++ directory_bruteforcing/dirbrute.py
@@ -6,19 +6,19 @@ file = open(path, "r")
 
 for i in range(len(file)):
     wls = file.readline()
-    r = requests.get(url+wls)
+    r = requests.get(url + wls)
     stats = r.status_code
 
 if stats == 200:
-    print("_"*50)
-    print("Path :"+url+wls, (stats))
-    print("_"*50)
+    print("_" * 50)
+    print("Path :"+url + wls, (stats))
+    print("_" * 50)
 elif stats == 404:
-    print("_"*50)
-    print("Path :"+url+wls, (stats))
-    print("_"*50)
+    print("_" * 50)
+    print("Path :" + url + wls, (stats))
+    print("_" * 50)
 else:
-    print("_"*50)
-    print("Path :"+url+wls, (stats))
-    print("_"*50)
+    print("_" * 50)
+    print("Path :" + url + wls, (stats))
+    print("_" * 50)
 file.close()
