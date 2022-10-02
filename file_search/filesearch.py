acc3b5cf1c36821471ea971d0e9f0fd5326c8786
TheSpecialCoder <55451896+visheshdvivedi@users.noreply.github.com>
2022-10-02 17:46:26 +0530

    formatted code in filesearch

--- file_search/filesearch.py
+++ file_search/filesearch.py
@@ -3,18 +3,19 @@ import datetime
 import argparse
 from progress.bar import Bar
 
+
 class FileSearch:
     def __init__(self):
         self.file_count = 0
         self.progress_bar = Bar('Searching', suffix='%(eta)ds')
 
     def get_file_count(self, path, is_folder):
-        
+
         if not is_folder:
             print("[INFO] Reading files ...")
         else:
             print("[INFO] Reading folders ...")
-        
+
         self.file_count = 0
         for root, dirs, files in os.walk(path):
             if is_folder:
@@ -22,7 +23,14 @@ class FileSearch:
             else:
                 self.file_count += len(files)
 
-    def __recursive_check(self, search_term, start_dir, is_folder, disable_case, show_bar):
+    def __perform_search(
+        self,
+        search_term,
+        start_dir,
+        is_folder,
+        disable_case,
+        show_bar
+    ):
         results = []
         for root, dirs, files in os.walk(start_dir):
             if is_folder:
@@ -48,7 +56,14 @@ class FileSearch:
 
         return results
 
-    def search(self, search_term, start_dir, is_folder=False, disable_case=False, show_bar=True):
+    def search(
+        self,
+        search_term,
+        start_dir,
+        is_folder=False,
+        disable_case=False,
+        show_bar=True
+    ):
         self.file_count = 0
         self.get_file_count(start_dir, is_folder)
         self.progress_bar.max = self.file_count
@@ -57,31 +72,72 @@ class FileSearch:
             raise Exception(f"Path {start_dir} does not exist.")
 
         start_time = datetime.datetime.now()
-        result = self.__recursive_check(search_term, start_dir, is_folder, disable_case, show_bar)
+        result = self.__perform_search(
+            search_term,
+            start_dir,
+            is_folder,
+            disable_case,
+            show_bar
+        )
         end_time = datetime.datetime.now()
 
         if not is_folder:
-            print(f"\n\n[INFO] Searched {self.file_count:,} files in {(end_time-start_time).total_seconds()} seconds")
+            print(
+                f"\n\n[INFO] Searched {self.file_count:,} files in {(end_time-start_time).total_seconds()} seconds")
         else:
-            print(f"\n\n[INFO] Searched {self.file_count:,} folders in {(end_time-start_time).total_seconds()} seconds")
-        
+            print(
+                f"\n\n[INFO] Searched {self.file_count:,} folders in {(end_time-start_time).total_seconds()} seconds")
+
         return result
+
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="Search files within your computer")
-    
-    parser.add_argument("search-term", metavar="term", type=str, help="Filename/Word to search for")
-    parser.add_argument("start-dir", metavar="dir", type=str, help="Directory where you want to search")
-    
-    parser.add_argument("-dc", "--disable-case", action="store_true", help="Perform case insensitive search")
-    parser.add_argument("-f", "--folder", action="store_true", help="Search only folders (default: searches files)")
-    parser.add_argument("-nb", "--no-bar", action="store_false", help="Not show progress bar")
-    
+    parser = argparse.ArgumentParser(
+        description="Search files within your computer")
+
+    parser.add_argument(
+        "search-term",
+        metavar="term",
+        type=str,
+        help="Filename/Word to search for"
+    )
+    parser.add_argument(
+        "start-dir",
+        metavar="dir",
+        type=str,
+        help="Directory where you want to search"
+    )
+
+    parser.add_argument(
+        "-dc",
+        "--disable-case",
+        action="store_true",
+        help="Perform case insensitive search"
+    )
+    parser.add_argument(
+        "-f",
+        "--folder",
+        action="store_true",
+        help="Search only folders (default: searches files)"
+    )
+    parser.add_argument(
+        "-nb",
+        "--no-bar",
+        action="store_false",
+        help="Not show progress bar"
+    )
+
     args = parser.parse_args().__dict__
 
     searcher = FileSearch()
-    results = searcher.search(args['search-term'], args['start-dir'], args['folder'], args['disable_case'], args['no_bar'])
+    results = searcher.search(
+        args['search-term'],
+        args['start-dir'],
+        args['folder'],
+        args['disable_case'],
+        args['no_bar']
+    )
 
     print("[INFO] Results: \n")
     for path in results:
