749c1a6f62920faf10446f5f4802da2753a4eb87
Prajwol-Shrestha <prajwolshrestha08@gmail.com>
2022-10-04 09:23:41 +0545

    fixed linting

--- auto_shutdown/auto_shutdown.py
+++ auto_shutdown/auto_shutdown.py
@@ -14,7 +14,7 @@ set_time = int(set_time)
 sec = 60
 
 print('Computer Will Now Shutdown in ' + str(set_time) + ' Minutes')
-time.sleep(set_time*sec)
+time.sleep(set_time * sec)
 print('\n')
 print('Computer Will Now Shutdown!')
 time.sleep(3)
