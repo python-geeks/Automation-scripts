150432958c774fb6842a31cb56a6955ef41c135a
Alexander Pitkin <peleccom@gmail.com>
2022-10-07 00:07:13 +0300

    Add option to call yaml_to_json with command line arguments

--- yaml_to_json/yaml_to_json.py
+++ yaml_to_json/yaml_to_json.py
@@ -1,9 +1,11 @@
 from ruyaml import YAML
+import argparse
 import json
 
 
-def get_yaml_data():
-    yaml_name = input("Enter the yaml file name: ")
+def get_yaml_data(yaml_name=None):
+    if not yaml_name:
+        yaml_name = input("Enter the yaml file name: ")
 
     try:
         with open(yaml_name, "r+") as f:
@@ -14,18 +16,34 @@ def get_yaml_data():
         yaml_data = get_yaml_data()
 
 
-def convert_to_json(yaml_data):
-    json_name = input("Enter the name of output json file: ")
+def convert_to_json(yaml_data, json_name=None, intent=None):
+    if not json_name:
+        json_name = input("Enter the name of output json file: ")
 
     try:
         with open(json_name, "w+") as o:
-            o.write(json.dumps(yaml_data))
+            o.write(json.dumps(yaml_data, indent=intent))
     except:  # noqa
         print("Invalid input enter a valid json file name e.g. example.json")
         convert_to_json(yaml_data)
 
 
-yaml_data = get_yaml_data()
-convert_to_json(yaml_data)
+def main():
+    parser = argparse.ArgumentParser(description='Convert YAML file to JSON')
+    parser.add_argument('--yaml', type=str, help='YAML filename')
+    parser.add_argument('--json', type=str, help='JSON filename')
+    parser.add_argument('--intent', type=int, help="intent value for JSON")
+    args = parser.parse_args()
 
-print("Your yaml file has been converted and saved as json")
+    yaml_name = args.yaml
+    json_name = args.json
+    intent = args.intent
+
+    yaml_data = get_yaml_data(yaml_name)
+    convert_to_json(yaml_data, json_name, intent=intent)
+
+    print("Your yaml file has been converted and saved as json")
+
+
+if __name__ == "__main__":
+    main()
