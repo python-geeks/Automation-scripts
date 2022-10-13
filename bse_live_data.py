3f58139c550d7baf92c5e13e32b8ef21cc7e081d
kashyap-ji <97298851+kashyap-ji@users.noreply.github.com>
2022-10-14 03:36:58 +0530

    Update bse_live_data.py

--- bse_live_data.py
+++ bse_live_data.py
@@ -3,24 +3,24 @@
 #start
 from bsedata.bse import BSE
 bse = BSE()
-print(bse)
+#print(bse)
 # to execute "updateScripCodes" on instantiation
 bse = BSE(update_codes = True)
 #to get quotes of companies
 codelist = ["500116", "512573"]
 for code in codelist:
     quote = bse.getQuote(code)
-    print(quote["companyName"])
-    print(quote["currentValue"])
-    print(quote["updatedOn"])
+    #print(quote["companyName"])
+    #print(quote["currentValue"])
+    #print(quote["updatedOn"])
 #top gainers
 top_gain = bse.topGainers()
-print(top_gain)
+#print(top_gain)
 #top loser
 top_loser = bse.topLosers()
-print(top_loser)
+#print(top_loser)
 #period trend of a specific company {here of 6 months}
 trend = bse.getPeriodTrend('534976','6M')
-print(trend)
+#print(trend)
 #all info
-print(bse.getScripCodes())
+#print(bse.getScripCodes())
