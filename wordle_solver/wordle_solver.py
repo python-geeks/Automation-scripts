980016c35b555c6b39ab0469405089f24aa678c0
Adrien PARAISO <adrien.paraiso@outlook.com>
2022-10-24 18:23:02 +0200

    Code Improvements

--- wordle_solver/wordle_solver.py
+++ wordle_solver/wordle_solver.py
@@ -15,22 +15,23 @@ import time
 
 # Check word length
 def check_word_length(word):
-    if len(word) != 6:
+    if len(word) != 5:
         return False
     else:
         return True
 
 
-list_of_words = open("words_alpha.txt", "r").read().splitlines()
+list_of_words = open("words_alpha.txt", "r").read().strip().splitlines()
 list_of_words = list(filter(check_word_length, list_of_words))
 
 
 def on_release(key):
+    # Start button
     if key == keyboard.Key.esc:
         return False  # stop listener
 
 
-def get_wordle_evaluation(game_row, browser):
+def get_row_results(game_row):
     tiles = game_row.find_elements(By.CLASS_NAME, "Tile-module_tile__3ayIZ")
     evaluation = []
     eval_to_int = {
@@ -46,8 +47,15 @@ def get_wordle_evaluation(game_row, browser):
     return tuple(evaluation)
 
 
+def enter_word(word):
+    keyboard_controller = keyboard.Controller()
+    keyboard_controller.type(word)
+    keyboard_controller.tap(keyboard.Key.enter)
+    time.sleep(2)
+
+
 def main():
-    start_button = 'esc'
+    # Start the browser
     browser = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
     browser.get("https://www.nytimes.com/games/wordle/index.html")
 
@@ -56,26 +64,18 @@ def main():
         listener.join()
     print("Starting")
 
-    first_string = "moron"
-    keyboard_controller = keyboard.Controller()
-    keyboard_controller.type(first_string)
-    keyboard_controller.tap(keyboard.Key.enter)
-
-    time.sleep(2)
-
+    # Get the game rows
     game_rows = browser.find_elements(By.CLASS_NAME, 'Row-module_row__dEHfN')
-    # print("Game rows: ", len(game_rows))
-    get_wordle_evaluation(game_rows[0], browser)
+
+    first_string = "tests"
+    enter_word(first_string)
+    get_row_results(game_rows[0])
 
     time.sleep(1)
 
-    second_string = "tests"
-    keyboard_controller.type(second_string)
-    keyboard_controller.tap(keyboard.Key.enter)
-
-    time.sleep(2)
-
-    get_wordle_evaluation(game_rows[1], browser)
+    second_string = "trees"
+    enter_word(second_string)
+    get_row_results(game_rows[1])
 
 
 #From the basic list of words, return all the words with 5 characters.
