617b8495ed8e636e5a877ef4cbb49f82c643f303
kkohli <kshitij_kohli@intuit.com>
2022-10-31 08:44:41 +0530

    fixing lint issues

--- eye_blink_reminder/eye_blink_reminder.py
+++ eye_blink_reminder/eye_blink_reminder.py
@@ -1,25 +1,23 @@
 import os
 import playsound
-import time
 from time import sleep
-import sys
 import multiprocessing
 
 
 def remind():
-	startTime = time.time()
-	while True:
-		sleep(20*60)
-		os.popen('osascript -e "set Volume 6"')
-		p = multiprocessing.Process(target=playsound.playsound, args=("danger.mp3",))
-		p.start()
-		inp = input('Dismiss? y|n')
+    while True:
+        sleep(20*60)
+        os.popen('osascript -e "set Volume 6"')
+        p = multiprocessing.Process(
+            target=playsound.playsound, args=("danger.mp3",))
+        p.start()
+        inp = input('Dismiss? y|n')
 
-		if inp == 'y':
-			print("yes")
-			p.terminate()
-			continue
+        if inp == 'y':
+            print("yes")
+            p.terminate()
+            continue
 
 
 if __name__ == "__main__":
-	remind()
+    remind()
