4f2dd031407081c7082ce68a4cfc44c822923b46
redxz5 <mridul240205@gmail.com>
2024-10-01 21:32:50 +0530

    flake8 linting

--- taylor_mclaurian_expansion/main.py
+++ taylor_mclaurian_expansion/main.py
@@ -14,15 +14,15 @@ def taylorexpansion(func, a, n, var):
         k = 1
         while (k < n):
             d = diff(d, var)
-            term = (d*((t_y-a)**k))/factorial(k)
+            term = (d * ((t_y - a) ** k)) / factorial(k)
             term = term.subs(var, a)
             if (term == 0):
                 continue
             term = term.subs(t_y, var)
-            expansion = term+expansion
+            expansion = term + expansion
             k += 1
             if (d == 0 and k < n):
-                print("only ", k-1, " terms present")
+                print("only ", k - 1, " terms present")
         if (n < 1):
             print("3rd argument is for no. of terms, provide a natural number")
             return ''
@@ -54,16 +54,16 @@ def examples():
     print(taylorvalue(exp(x), 1, 10, x))
 
     # log(1+x) expansion at x=0 with 5 terms differentiating with respect to x
-    pprint(taylorexpansion(log(x+1), 0, 5, x))
+    pprint(taylorexpansion(log(x + 1), 0, 5, x))
 
     # sin(x) expansion at x=0 with 5 terms differentiating with respect to x
     pprint(taylorexpansion(sin(x), 0, 5, x))
 
     # expansion for expression at x=0 with 3 terms differentiating wrt to x
-    pprint(taylorexpansion((5*x**2)+(3*x)+(7), 0, 3, x))
+    pprint(taylorexpansion((5 * x ** 2) + (3 * x) + (7), 0, 3, x))
 
     # e^(xy) expansion at x=1 with 5 terms differentiating with respect to x
-    pprint(taylorexpansion(exp(x*y), 1, 5, x))
+    pprint(taylorexpansion(exp(x * y), 1, 5, x))
 
 
 examples()
