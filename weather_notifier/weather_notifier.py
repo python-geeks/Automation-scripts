1dfea5e8fc8e08286d36ec30bb9139e7a1f78497
DavdaJames <jrdavda007@gmail.com>
2024-10-02 13:01:25 +0530

    flake8 linting added

--- weather_notifier/weather_notifier.py
+++ weather_notifier/weather_notifier.py
@@ -1,32 +1,41 @@
-import tkinter as tk 
+import os
+from geopy.geocoders import Nominatim
+import tkinter as tk
 from tkinter import messagebox
 import requests
 import threading
 import time
-from dotenv import load_dotenv 
+from dotenv import load_dotenv
 load_dotenv()
-from geopy.geocoders import Nominatim
-import os
 
-API_KEY=os.getenv('OPENWEATHER_API_KEY')
+API_KEY = os.getenv('OPENWEATHER_API_KEY')
 if API_KEY is None:
-    raise ValueError("API key not found. Please set the OPENWEATHER_API_KEY in the .env file.")
+    raise ValueError(
+        "API key not found.  Set the OPENWEATHER_API_KEY in the .env file.")
+
 
 def calculate_lat_long(city):
     geolocator = Nominatim(user_agent="weather_notifier")
     location = geolocator.geocode(city)
     if location:
         return location.latitude, location.longitude
-    return None,None
-def get_weather(lat,long):
+    return None, None
+
+
+def get_weather(lat, long):
     try:
-        print(lat,long)
-        url=f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={long}&units=metric&appid={API_KEY}"
-        res=requests.get(url)
+        print(lat, long)
+        url = (
+            f"https://api.openweathermap.org/data/2.5/weather?lat={lat}"
+            f"&lon={long}&units=metric&appid={API_KEY}"
+        )
+        res = requests.get(url)
         data = res.json()
         if data['cod'] != 404:
             weather_info = {
-                "City": data.get("name", "N/A"),
+                "City": data.get(
+                    "name",
+                    "N/A"),
                 "Latitude": data['coord']['lat'],
                 "Longitude": data['coord']['lon'],
                 "Temperature": data['main']['temp'],
@@ -38,35 +47,49 @@ def get_weather(lat,long):
                 "Wind Speed": data['wind']['speed'],
                 "Wind Degree": data['wind']['deg'],
                 "Weather": data['weather'][0]['description'].capitalize(),
-                "Clouds": f"{data['clouds']['all']}%",
-                "Visibility": data.get('visibility', "N/A"),
-                "Sunrise": time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(data['sys']['sunrise'] + data['timezone'])),
-                "Sunset": time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(data['sys']['sunset'] + data['timezone'])),
+                "Clouds": f"{
+                    data['clouds']['all']}%",
+                "Visibility": data.get(
+                    'visibility',
+                    "N/A"),
+                "Sunrise": time.strftime(
+                    '%Y-%m-%d %H:%M:%S',
+                    time.gmtime(
+                        data['sys']['sunrise'] + data['timezone'])),
+                "Sunset": time.strftime(
+                    '%Y-%m-%d %H:%M:%S',
+                    time.gmtime(
+                        data['sys']['sunset'] + data['timezone'])),
             }
             return weather_info
         else:
             return None
-        
+
     except Exception as e:
         messagebox.showerror("Error", f"Error fetching weather data: {str(e)}")
         return None
 
-# updating the weather 
+# updating the weather
+
+
 def update_weather():
-    city=city_entry.get()
+    city = city_entry.get()
     if city:
         lat, lon = calculate_lat_long(city)
         if lat and lon:
             weather_info = get_weather(lat, lon)
             if weather_info:
-                weather_info_in_str_to_display=covert_the_info_to_display(weather_info)
+                weather_info_in_str_to_display = covert_the_info_to_display(
+                    weather_info)
                 weather_label.config(text=weather_info_in_str_to_display)
                 stop_button.pack(pady=5)
-                city_label.pack_forget()    
+                city_label.pack_forget()
                 city_entry.pack_forget()
-                manual_radio.pack_forget()  
-                auto_radio.pack_forget() 
+                manual_radio.pack_forget()
+                auto_radio.pack_forget()
                 start_button.pack_forget()
+                interval_label.pack_forget()
+                interval_entry.pack_forget()
             else:
                 weather_label.config(text="Unable to find coordinates!")
                 stop_button.pack_forget()
@@ -74,60 +97,78 @@ def update_weather():
             weather_label.config(text="Unable to find coordinates!")
             stop_button.pack_forget()
     else:
-       messagebox.showerror("Error", "Please enter a valid city name.")
-       stop_button.pack_forget()
-#  displaying the info in the tkinter created box 
+        messagebox.showerror("Error", "Please enter a valid city name.")
+        stop_button.pack_forget()
+#  displaying the info in the tkinter created box
+
+
 def covert_the_info_to_display(weather_info):
     # Clear the previous text
-    weather_info_in_str_to_display= f'''
-                    City: {weather_info['City']}\n
-                    Coordinates: ({weather_info['Latitude']}, {weather_info['Longitude']})\n
-                    Temperature: {weather_info['Temperature']}°C (Feels like {weather_info['Feels Like']}°C)\n
-                    Min Temp: {weather_info['Min Temp']}°C, Max Temp: {weather_info['Max Temp']}°C\n
-                    Pressure: {weather_info['Pressure']} hPa\n
-                    Humidity: {weather_info['Humidity']}%\n
-                    Wind: {weather_info['Wind Speed']} m/s, {weather_info['Wind Degree']}°\n
-                    Clouds: {weather_info['Clouds']}\n
-                    Visibility: {weather_info['Visibility']} meters\n
-                    Weather: {weather_info['Weather']}\n
-                    Sunrise: {weather_info['Sunrise']}\n
-                    Sunset: {weather_info['Sunset']}\n
-                    '''
+    weather_info_in_str_to_display = (
+        f"City: {weather_info['City']}\n\n"
+        f"Coordinates: ({weather_info['Latitude']}, "
+        f"{weather_info['Longitude']})\n\n"
+        f"Temperature: {weather_info['Temperature']}°C "
+        f"(Feels like {weather_info['Feels Like']}°C)\n\n"
+        f"Min Temp: {weather_info['Min Temp']}°C, "
+        f"Max Temp: {weather_info['Max Temp']}°C\n\n"
+        f"Pressure: {weather_info['Pressure']} hPa\n\n"
+        f"Humidity: {weather_info['Humidity']}%\n\n"
+        f"Wind: {weather_info['Wind Speed']} m/s, "
+        f"{weather_info['Wind Degree']}°\n\n"
+        f"Clouds: {weather_info['Clouds']}\n\n"
+        f"Visibility: {weather_info['Visibility']} meters\n\n"
+        f"Weather: {weather_info['Weather']}\n\n"
+        f"Sunrise: {weather_info['Sunrise']}\n\n"
+        f"Sunset: {weather_info['Sunset']}\n\n"
+    )
+
     return weather_info_in_str_to_display
 
 # run_in_background logic
+
+
 def run_in_background(interval):
     while auto_mode.get():
         update_weather()
         time.sleep(interval)
 
 #  Function to handle click
+
+
 def start_notifier():
     if auto_mode.get():
         interval_str = interval_entry.get().strip()
         if not interval_str:
-            messagebox.showerror("Error", "Please enter a valid interval (in seconds).")
-            return  
+            messagebox.showerror(
+                "Error", "Please enter a valid interval (in seconds).")
+            return
         try:
-            interval=int(interval_str)
+            interval = int(interval_str)
             if interval <= 0:
-                messagebox.showerror("Error", "Please enter a valid interval (in seconds).")
+                messagebox.showerror(
+                    "Error", "Please enter a valid interval (in seconds).")
                 return
-        except ValueError as e:
+        except ValueError:
             messagebox.showerror("Error", "Please enter a valid number.")
             return
         start_button.config(state=tk.DISABLED)
 
-        threading.Thread(target=run_in_background, args=(interval,), daemon=True).start()
+        threading.Thread(
+            target=run_in_background, args=(
+                interval,), daemon=True).start()
     else:
         update_weather()
 
 # Function to stop auto-updating
+
+
 def stop_notifier():
     auto_mode.set(False)
     start_button.config(state=tk.NORMAL)
-    stop_button.pack_forget()  
+    stop_button.pack_forget()
     go_back()
+
 
 def go_back():
     weather_label.config(text="")
@@ -139,15 +180,18 @@ def go_back():
     interval_label.pack_forget()
     interval_entry.pack_forget()
     stop_button.pack_forget()
-    
+
 # gui setup
+
+
 def show_interval_entry():
-        if auto_mode.get():
-            interval_label.pack(pady=5)
-            interval_entry.pack(pady=5)
-        else:
-            interval_label.pack_forget()
-            interval_entry.pack_forget()
+    if auto_mode.get():
+        interval_label.pack(pady=5)
+        interval_entry.pack(pady=5)
+    else:
+        interval_label.pack_forget()
+        interval_entry.pack_forget()
+
 
 def toggle_stop_button():
     if auto_mode.get():
@@ -155,13 +199,14 @@ def toggle_stop_button():
     else:
         stop_button.pack_forget()
 
-if __name__=='__main__':
-    city ="Surat"
-    lat,long=calculate_lat_long(city)
-    if lat==None or long==None:
+
+if __name__ == '__main__':
+    city = "Surat"
+    lat, long = calculate_lat_long(city)
+    if lat is None or long is None:
         print('No city found')
         exit(0)
-    
+
     root = tk.Tk()
     root.title("Weather Notifier")
     root.geometry("550x500")
@@ -174,28 +219,39 @@ if __name__=='__main__':
     city_entry.pack(pady=5)
 
     # Weather Info Label
-    weather_label = tk.Label(root, text="", font=("Helvetica", 10),justify="left")
+    weather_label = tk.Label(
+        root, text="", font=(
+            "Helvetica", 10), justify="left")
     weather_label.pack(pady=20)
 
     # Mode Selection: Manual or Automatic
     auto_mode = tk.BooleanVar()
 
-    manual_radio = tk.Radiobutton(root, text="On-click only", variable=auto_mode, value=False)
+    manual_radio = tk.Radiobutton(
+        root,
+        text="On-click only",
+        variable=auto_mode,
+        value=False)
     manual_radio.pack(anchor=tk.W, padx=20)
 
-    auto_radio = tk.Radiobutton(root, text="Run after a fixed interval", variable=auto_mode, value=True)
+    auto_radio = tk.Radiobutton(
+        root,
+        text="Run after a fixed interval",
+        variable=auto_mode,
+        value=True)
     auto_radio.pack(anchor=tk.W, padx=20)
 
     # Interval Entry (only visible when interval mode is selected)
     interval_label = tk.Label(root, text="Enter interval (seconds):")
     interval_entry = tk.Entry(root, width=10)
 
-    
-
     auto_mode.trace_add("write", lambda *args: show_interval_entry())
 
     # Start Button
-    start_button = tk.Button(root, text="Start Notifier", command=start_notifier)
+    start_button = tk.Button(
+        root,
+        text="Start Notifier",
+        command=start_notifier)
     start_button.pack(pady=10)
 
     # Stop Button (visible only when auto mode is active)
