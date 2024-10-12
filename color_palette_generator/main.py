57268b113f3548f221dc91fd0cbceb525da0c93e
Tushar Satpute <tusharsatpute68@gmail.com>
2024-10-12 20:25:12 +0530

    Modified For Flake8

--- color_palette_generator/main.py
+++ color_palette_generator/main.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 import random
 
-
 app = Flask(__name__)
 
 
@@ -27,4 +26,3 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
